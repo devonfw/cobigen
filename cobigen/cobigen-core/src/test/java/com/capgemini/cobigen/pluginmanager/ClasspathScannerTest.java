2698fd7be63ee839264eadbf0011262de416d451
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2014-08-05 15:39:51 +0200

    #19: Ignored test for not yet implemented logic

--- cobigen/cobigen-core/src/test/java/com/capgemini/cobigen/pluginmanager/ClasspathScannerTest.java
+++ cobigen/cobigen-core/src/test/java/com/capgemini/cobigen/pluginmanager/ClasspathScannerTest.java
@@ -1,11 +1,13 @@
 package com.capgemini.cobigen.pluginmanager;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ClasspathScannerTest {
 
     @Test
+    @Ignore("Logic not yet implemented")
     public void testePluginRegistering() throws ClassNotFoundException {
 
         // getClass().getClassLoader().loadClass("ClasspathScannerTest.TestMerger");
