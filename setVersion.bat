583653bae5d38f14a0ca4ea8554287c04b05ac8f
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2014-08-26 08:53:06 +0200

    maven build scripts added

---
+++ setVersion.bat
@@ -0,0 +1,2 @@
+call mvn -Dtycho.mode=maven org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=%1
+pause
---
+++ setup_eclipse.bat
@@ -0,0 +1,2 @@
+call mvn clean install -Dtest=false -DfailIfNoTests=false eclipse:clean eclipse:eclipse -U
+pause
