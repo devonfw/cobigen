56211010edcfd8465451b17e8f4547a0226bba4c
Fabian Kreis <FabianKreis@gmx.de>
2014-09-25 14:21:40 +0200

    added test cases for new model contents (super types) for ReflectedJavaModelBuilder

--- cobigen/cobigen-javaplugin/src/test/java/com/capgemini/cobigen/javaplugin/inputreader/ReflectedJavaModelBuilderTest.java
+++ cobigen/cobigen-javaplugin/src/test/java/com/capgemini/cobigen/javaplugin/inputreader/ReflectedJavaModelBuilderTest.java
@@ -1,5 +1,6 @@
 package com.capgemini.cobigen.javaplugin.inputreader;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ import org.junit.Test;
 
 /**
  * Tests for Class {@link ReflectedJavaModelBuilder}
- * 
+ *
  * @author <a href="m_brunnl@cs.uni-kl.de">Malte Brunnlieb</a>
  * @version $Revision$
  */
@@ -44,8 +45,35 @@ public class ReflectedJavaModelBuilderTest {
         }
 
         Assert.assertNotNull("There is no field with name 'parametricTestAttribute' in the model",
-                parametricTestAttribute);
+            parametricTestAttribute);
         // "List<String>" is not possible to retrieve using reflection due to type erasure
         Assert.assertEquals("List<?>", parametricTestAttribute.get(ModelConstant.TYPE));
+    }
+
+    /**
+     * Tests whether supertypes (extended Type and implemented Types) will be extracted correctly to the model
+     *
+     */
+    @Test
+    public void testCorrectlyExtractedSuperTypes() {
+
+        ReflectedJavaModelBuilder javaModelBuilder = new ReflectedJavaModelBuilder();
+        Map<String, Object> model = javaModelBuilder.createModel(getClass());
+
+        // check whether extended Type meets expectations
+        @SuppressWarnings("unchecked")
+        Map<String, Object> pojoMap = (Map<String, Object>) model.get(ModelConstant.ROOT);
+        Assert.assertNotNull(ModelConstant.ROOT + " is not accessible in model", pojoMap);
+        @SuppressWarnings("unchecked")
+        Map<String, Object> supertype = (Map<String, Object>) pojoMap.get(ModelConstant.EXTENDED_TYPE);
+        Assert.assertNotNull(ModelConstant.EXTENDED_TYPE + " is not accessible in model", supertype);
+        System.out.println(supertype);
+        Assert.assertEquals(supertype.get(ModelConstant.NAME), "java.lang.Object");
+        Assert.assertEquals(supertype.get(ModelConstant.CANONICAL_NAME), "java.lang.Object");
+        Assert.assertEquals(supertype.get(ModelConstant.PACKAGE), "java.lang");
+
+        // check whether implemented Types (interfaces) meet expectations (should be empty list)
+        Assert.assertEquals(new LinkedList<Map<String, Object>>(),
+            pojoMap.get(ModelConstant.IMPLEMENTED_TYPES));
     }
 }
