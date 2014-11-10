9bee41753a1ae87c835bc4b6475a64b17e31747d
Fabian Kreis <fabian.kreis@capgemini.com>
2014-11-10 17:24:43 +0100

    #50 added (unit) test cases for XmlInputReader

---
+++ cobigen/cobigen-xmlplugin/src/test/java/com/capgemini/cobigen/xmlplugin/test/XmlInputReaderTest.java
@@ -0,0 +1,46 @@
+package com.capgemini.cobigen.xmlplugin.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.dom4j.dom.DOMDocument;
+import org.junit.Test;
+
+import com.capgemini.cobigen.xmlplugin.inputreader.XmlInputReader;
+
+/**
+ *
+ * @author fkreis (10.11.2014)
+ */
+public class XmlInputReaderTest {
+
+    /**
+     * Test method for {@link XmlInputReader#isValidInput(java.lang.Object)} in case of a valid input.
+     */
+    @Test
+    public void testIsValidInput_isValid() {
+        XmlInputReader xmlInputReader = new XmlInputReader();
+        DOMDocument validInput = new DOMDocument();
+        assertTrue(xmlInputReader.isValidInput(validInput));
+    }
+
+    /**
+     * Test method for {@link XmlInputReader#isValidInput(java.lang.Object)} in case of an invalid input.
+     */
+    @Test
+    public void testIsValidInput_isNotValid() {
+        XmlInputReader xmlInputReader = new XmlInputReader();
+        Object invalidInput = new Object();
+        assertFalse(xmlInputReader.isValidInput(invalidInput));
+    }
+
+    /**
+     * Test method for {@link XmlInputReader#createModel(java.lang.Object)}.
+     */
+    @Test
+    public void testCreateModel() {
+        fail("Not yet implemented");
+    }
+
+}
