e9f80b88e775444d34414334ee991d7109bcccbb
Steffen Holzer <steffenbholzer@googlemail.com>
2015-12-22 18:08:29 +0100

    #154 Added a new Enum MergeType that translates the cobigen merge strategies in XmlLawMerge strategies. Enum integrated and tested. All passed

---
+++ cobigen/cobigen-xmlplugin/src/main/java/com/capgemini/cobigen/xmlplugin/merger/delegates/MergeType.java
@@ -0,0 +1,72 @@
+package com.capgemini.cobigen.xmlplugin.merger.delegates;
+
+import com.capgemini.xmllawmerger.ConflictHandlingType;
+
+/**
+ * Maps the ConflictHandlingTypes and the already used merge types
+ * @author sholzer (Dec 22, 2015)
+ */
+public enum MergeType {
+
+    /**
+     * In case of a conflict the patch document is preferred
+     */
+    PATCHOVERWRITE("xml_merge_overwrite", ConflictHandlingType.PATCHOVERWRITE),
+    /**
+     * In case of a conflict the base document is preferred
+     */
+    BASEOVERWRITE("xml_merge", ConflictHandlingType.BASEOVERWRITE),
+    /**
+     * In case of a conflict the base document is preferred. Attributes and text nodes will be attached where
+     * possible
+     */
+    BASEATTACHOROVERWRITE("xml_merge_attachTexts", ConflictHandlingType.BASEATTACHOROVERWRITE),
+    /**
+     * In case of a conflict the patch document is preferred. Attributes and text nodes will be attached where
+     * possible
+     */
+    PATCHATTACHOROVERWRITE("xml_merge_overwrite_attachTexts", ConflictHandlingType.PATCHATTACHOROVERWRITE);
+    ;
+
+    /**
+     * The ConflictHandlingType
+     */
+    public ConflictHandlingType type;
+
+    /**
+     * The name of the merge mode
+     */
+    public String value;
+
+    /**
+     *
+     * @param value
+     *            the name of the merge mode
+     * @param type
+     *            the ConflictHandlingType
+     * @author sholzer (Dec 22, 2015)
+     */
+    private MergeType(String value, ConflictHandlingType type) {
+        this.type = type;
+        this.value = value;
+    }
+
+    /**
+     * Returns the field 'type'
+     * @return value of type
+     * @author sholzer (Dec 22, 2015)
+     */
+    public ConflictHandlingType getType() {
+        return type;
+    }
+
+    /**
+     * Returns the field 'value'
+     * @return value of value
+     * @author sholzer (Dec 22, 2015)
+     */
+    public String getValue() {
+        return value;
+    }
+
+}
