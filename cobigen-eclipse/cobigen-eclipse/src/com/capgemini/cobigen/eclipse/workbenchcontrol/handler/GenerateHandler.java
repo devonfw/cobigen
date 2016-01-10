b67b79a9840968a94a740638b06680bf9f5741e8
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2016-01-10 04:52:56 +0100

    #156: Cleanup input validation

--- cobigen-eclipse/cobigen-eclipse/src/com/capgemini/cobigen/eclipse/workbenchcontrol/handler/GenerateHandler.java
+++ cobigen-eclipse/cobigen-eclipse/src/com/capgemini/cobigen/eclipse/workbenchcontrol/handler/GenerateHandler.java
@@ -1,44 +1,26 @@
 package com.capgemini.cobigen.eclipse.workbenchcontrol.handler;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 import java.util.UUID;
-
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.ITreeSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
-import com.capgemini.cobigen.config.entity.Trigger;
 import com.capgemini.cobigen.eclipse.common.constants.InfrastructureConstants;
 import com.capgemini.cobigen.eclipse.common.constants.ResourceConstants;
 import com.capgemini.cobigen.eclipse.common.exceptions.GeneratorCreationException;
 import com.capgemini.cobigen.eclipse.common.exceptions.GeneratorProjectNotExistentException;
 import com.capgemini.cobigen.eclipse.common.exceptions.InvalidInputException;
-import com.capgemini.cobigen.eclipse.common.tools.ClassLoaderUtil;
-import com.capgemini.cobigen.eclipse.common.tools.JavaModelUtil;
 import com.capgemini.cobigen.eclipse.common.tools.PlatformUIUtil;
 import com.capgemini.cobigen.eclipse.generator.CobiGenWrapper;
 import com.capgemini.cobigen.eclipse.generator.GeneratorWrapperFactory;
@@ -48,8 +30,6 @@ import com.capgemini.cobigen.exceptions.InvalidConfigurationException;
 import com.capgemini.cobigen.exceptions.UnknownContextVariableException;
 import com.capgemini.cobigen.exceptions.UnknownExpressionException;
 import com.capgemini.cobigen.exceptions.UnknownTemplateException;
-import com.capgemini.cobigen.javaplugin.inputreader.to.PackageFolder;
-import com.capgemini.cobigen.xmlplugin.util.XmlUtil;
 
 /**
  * Handler for the Package-Explorer Event
@@ -74,18 +54,17 @@ public class GenerateHandler extends AbstractHandler {
 
         ISelection sel = HandlerUtil.getCurrentSelection(event);
 
-        if (sel instanceof ITreeSelection) {
+        if (sel instanceof IStructuredSelection) {
 
             // when this handler is executed, we should we should be sure, that the selection is currently
             // supported by the following implementation
 
             try {
-                checkValidSelection(sel);
                 CobiGenWrapper generator =
                     GeneratorWrapperFactory.createGenerator((IStructuredSelection) sel);
-                if (generator == null) {
+                if (generator == null || !generator.isValidInput((IStructuredSelection) sel)) {
                     MessageDialog.openError(HandlerUtil.getActiveShell(event), "Not yet supported!",
-                        "The selection is currently not supported as valid input.");
+                        "The current selection is currently not supported as valid input.");
                     return null;
                 }
 
@@ -141,125 +120,4 @@ public class GenerateHandler extends AbstractHandler {
         MDC.remove(InfrastructureConstants.CORRELATION_ID);
         return null;
     }
-
-    /**
-     * Checks if a selection is a valid selection. Adapted from
-     * {@link com.capgemini.cobigen.eclipse.workbenchcontrol.SelectionServiceListener#selectionChanged(org.eclipse.ui.IWorkbenchPart, ISelection)}
-     * @param sel
-     *            the selection to be validated
-     * @author sholzer (Sep 23, 2015)
-     * @throws InvalidInputException
-     *             if the selection can't be validated
-     * @throws GeneratorCreationException
-     *             if the used CobigenWrapper couldn't be instantiated
-     * @throws GeneratorProjectNotExistentException
-     *             if the used CobigenWrapper couldn't be instantiated
-     */
-    public void checkValidSelection(ISelection sel) throws InvalidInputException,
-        GeneratorProjectNotExistentException, GeneratorCreationException {
-        if (sel instanceof IStructuredSelection) {
-            IStructuredSelection selection = (IStructuredSelection) sel;
-            CobiGenWrapper cobiGen = GeneratorWrapperFactory.createGenerator(selection);
-            Iterator<?> it = selection.iterator();
-            List<String> firstTriggers = null;
-
-            boolean uniqueSourceSelected = false;
-
-            while (it.hasNext()) {
-                Object tmp = it.next();
-                if (tmp instanceof ICompilationUnit) {
-                    if (firstTriggers == null) {
-                        firstTriggers = findMatchingTriggers((ICompilationUnit) tmp, cobiGen);
-                    } else {
-                        if (!firstTriggers.equals(findMatchingTriggers((ICompilationUnit) tmp, cobiGen))) {
-                            throw new InvalidInputException(
-                                "You selected at least two inputs, which are not matching the same triggers. "
-                                    + "For batch processing all inputs have to match the same triggers.");
-                        }
-                    }
-                } else if (tmp instanceof IPackageFragment) {
-                    uniqueSourceSelected = true;
-                    firstTriggers =
-                        cobiGen.getMatchingTriggerIds(new PackageFolder(((IPackageFragment) tmp)
-                            .getResource().getLocationURI(), ((IPackageFragment) tmp).getElementName()));
-                } else if (tmp instanceof IFile) {
-                    uniqueSourceSelected = true;
-                    try (InputStream stream = ((IFile) tmp).getContents()) {
-                        LOG.debug("Try parsing file {} as xml...", ((IFile) tmp).getName());
-                        Document domDocument = XmlUtil.parseXmlStreamToDom(stream);
-                        firstTriggers = cobiGen.getMatchingTriggerIds(domDocument);
-                    } catch (CoreException e) {
-                        throw new InvalidInputException("An eclipse internal exception occured! ", e);
-                    } catch (IOException e) {
-                        throw new InvalidInputException("The file " + ((IFile) tmp).getName()
-                            + " could not be read!", e);
-                    } catch (ParserConfigurationException e) {
-                        throw new InvalidInputException("The file " + ((IFile) tmp).getName()
-                            + " could not be parsed, because of an internal configuration error!", e);
-                    } catch (SAXException e) {
-                        throw new InvalidInputException(
-                            "The contents of the file "
-                                + ((IFile) tmp).getName()
-                                + " could not be detected as an instance of any CobiGen supported input language.");
-                    }
-                } else {
-                    throw new InvalidInputException(
-                        "You selected at least one input, which type is currently not supported as input for generation. "
-                            + "Please choose a different one or read the CobiGen documentation for more details.");
-                }
-
-                if (uniqueSourceSelected && selection.size() > 1) {
-                    throw new InvalidInputException(
-                        "You selected at least one input in a mass-selection,"
-                            + " which type is currently not supported for batch processing. "
-                            + "Please just select multiple inputs only if batch processing is supported for all inputs.");
-                }
-            }
-            if (firstTriggers == null || firstTriggers.isEmpty()) {
-                throw new InvalidInputException("Could not find matching triggers for the current selection");
-            }
-        } else {
-            throw new InvalidInputException(
-                "The current selection is not an instance of IStructuredSelection");
-        }
-    }
-
-    /**
-     * adapted from {@link com.capgemini.cobigen.eclipse.workbenchcontrol.SelectionServiceListener}
-     * @param cu
-     *            {@link ICompilationUnit} to be checked
-     * @param cobiGen
-     *            the CobigenWrapper to be used for test generation
-     * @return the {@link Set} of {@link Trigger}s
-     * @throws InvalidInputException
-     *             if the input could not be read as expected
-     * @author trippl (22.04.2013), adapted by sholzer (28.09.2015)
-     */
-    private List<String> findMatchingTriggers(ICompilationUnit cu, CobiGenWrapper cobiGen)
-        throws InvalidInputException {
-
-        ClassLoader classLoader;
-        IType type = null;
-        try {
-            classLoader = ClassLoaderUtil.getProjectClassLoader(cu.getJavaProject());
-            type = JavaModelUtil.getJavaClassType(cu);
-            return cobiGen.getMatchingTriggerIds(classLoader.loadClass(type.getFullyQualifiedName()));
-        } catch (MalformedURLException e) {
-            throw new InvalidInputException("Error while retrieving the project's ('"
-                + cu.getJavaProject().getElementName() + "') classloader.", e);
-        } catch (CoreException e) {
-            throw new InvalidInputException("An eclipse internal exception occured!", e);
-        } catch (ClassNotFoundException e) {
-            throw new InvalidInputException("The class '" + type.getFullyQualifiedName()
-                + "' could not be found. "
-                + "This may be cause of a non-compiling host project of the selected input.", e);
-        } catch (UnsupportedClassVersionError e) {
-            throw new InvalidInputException(
-                "Incompatible java version: "
-                    + "You have selected a java class, which Java version is higher than the Java runtime your eclipse is running with. "
-                    + "Please update your PATH variable to reference the latest Java runtime you are developing for and restart eclipse.\n"
-                    + "Current runtime: " + System.getProperty("java.version"), e);
-        }
-    }
-
 }
