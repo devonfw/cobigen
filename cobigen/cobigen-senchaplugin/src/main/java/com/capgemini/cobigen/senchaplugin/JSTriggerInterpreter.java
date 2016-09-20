92d60b54b4a78d3240aac3e640af234865c716b8
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2016-09-20 12:25:26 +0200

    ID arch generation preview

--- cobigen/cobigen-senchaplugin/src/main/java/com/capgemini/cobigen/senchaplugin/JSTriggerInterpreter.java
+++ cobigen/cobigen-senchaplugin/src/main/java/com/capgemini/cobigen/senchaplugin/JSTriggerInterpreter.java
@@ -1,5 +1,7 @@
 package com.capgemini.cobigen.senchaplugin;
 
+import java.util.Random;
+
 import com.capgemini.cobigen.api.extension.InputReader;
 import com.capgemini.cobigen.api.extension.MatcherInterpreter;
 import com.capgemini.cobigen.api.extension.TriggerInterpreter;
@@ -16,6 +18,14 @@ public class JSTriggerInterpreter implements TriggerInterpreter {
      * {@link TriggerInterpreter} type to be registered
      */
     public String type;
+
+    private String modelId;
+
+    private String controllerId;
+
+    private String storeId;
+
+    private String viewId;
 
     /**
      * Creates a new Java Interpreter
@@ -42,7 +52,7 @@ public class JSTriggerInterpreter implements TriggerInterpreter {
      */
     @Override
     public InputReader getInputReader() {
-        return new JSInputReader();
+        return new JSInputReader(getModelId(), getControllerId(), getStoreId(), getViewId());
     }
 
     /**
@@ -52,5 +62,56 @@ public class JSTriggerInterpreter implements TriggerInterpreter {
     @Override
     public MatcherInterpreter getMatcher() {
         return new JSMatcher();
+    }
+
+    /**
+     * Generates random hexadecimal ID for Architect objects
+     * @param length
+     *            of the ID
+     * @return id
+     * @author rudiazma (Sep 19, 2016)
+     */
+    public static String createRandomString(int length) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length) {
+            sb.append(Integer.toHexString(random.nextInt()));
+        }
+        String id = sb.toString();
+        String resultId = id.substring(0, 8) + '-' + id.substring(8, 12) + '-' + id.substring(12, 16) + '-'
+            + id.substring(16, 20) + '-' + id.substring(20, 32);
+        return resultId;
+    }
+
+    public String getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(String modelId) {
+        this.modelId = modelId;
+    }
+
+    public String getControllerId() {
+        return controllerId;
+    }
+
+    public void setControllerId(String controllerId) {
+        this.controllerId = controllerId;
+    }
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getViewId() {
+        return viewId;
+    }
+
+    public void setViewId(String viewId) {
+        this.viewId = viewId;
     }
 }
