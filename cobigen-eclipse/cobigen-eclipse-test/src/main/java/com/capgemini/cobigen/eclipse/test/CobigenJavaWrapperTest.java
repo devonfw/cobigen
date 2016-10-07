f8b96021902f8608ff25b5c955495e980f6c9e00
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2016-10-07 10:24:21 +0200

    #164 fix classpath builder to allow external project locations and new system test for this

--- cobigen-eclipse/cobigen-eclipse-test/src/main/java/com/capgemini/cobigen/eclipse/test/CobigenJavaWrapperTest.java
+++ cobigen-eclipse/cobigen-eclipse-test/src/main/java/com/capgemini/cobigen/eclipse/test/CobigenJavaWrapperTest.java
@@ -7,7 +7,9 @@ import java.io.File;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.ui.JavaUI;
@@ -16,11 +18,13 @@ import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotPerspective;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
 import com.capgemini.cobigen.eclipse.common.constants.external.ResourceConstants;
@@ -35,26 +39,39 @@ import com.capgemini.cobigen.eclipse.test.common.swtbot.AllJobsAreFinished;
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class CobigenJavaWrapperTest {
 
-    /**
-     * Rule for creating temporary {@link IJavaProject}s per test.
-     */
+    /** Rule for creating temporary {@link IJavaProject}s per test. */
     @Rule
     public TmpMavenProjectRule tmpMavenProjectRule = new TmpMavenProjectRule();
+
+    /** Rule for creating temporary files and folders */
+    @Rule
+    public TemporaryFolder tmpFolderRule = new TemporaryFolder();
 
     /** Root path of the Test Resources */
     private static final String resourcesRootPath = "src/main/resources/CobiGenJavaWrapperTest/";
 
     /** {@link SWTBot} for UI controls */
-    private SWTWorkbenchBot bot = new SWTWorkbenchBot();
+    private static SWTWorkbenchBot bot = new SWTWorkbenchBot();
 
     /**
-     * Activate Java Perspective before each Test
+     * Setup workbench appropriately for tests
      */
-    @Before
-    public void setup() {
+    @BeforeClass
+    public static void setupClass() {
+        try {
+            bot.viewByTitle("Welcome").close();
+        } catch (WidgetNotFoundException e) {
+            // ignore
+        }
+
         SWTBotPerspective perspective = bot.perspectiveById(JavaUI.ID_PERSPECTIVE);
         perspective.activate();
-        bot.viewByTitle("Welcome").close();
+
+        // import the configuration project for this test
+        EclipseUtils.importExistingGeneralProject(bot,
+            new File(resourcesRootPath + "templates").getAbsolutePath());
+        EclipseUtils.updateMavenProject(bot, ResourceConstants.CONFIG_PROJECT_NAME);
+
         // this flag is set to be true and will suppress ErrorDialogs,
         // which is completely strange, so we enable them again.
         ErrorDialog.AUTOMATED_MODE = false;
@@ -73,22 +90,14 @@ public class CobigenJavaWrapperTest {
         IJavaProject project = tmpMavenProjectRule.createProject(testProjectName);
         tmpMavenProjectRule.createPom(
             // @formatter:off
-          "<dependencies>"
-            + "<dependency>"
-              + "<groupId>javax.ws.rs</groupId>"
-              + "<artifactId>javax.ws.rs-api</artifactId>"
-              + "<version>2.0</version>"
-            + "</dependency>"
-          + "</dependencies>");
+            "<dependencies>" + "<dependency>" + "<groupId>javax.ws.rs</groupId>"
+                + "<artifactId>javax.ws.rs-api</artifactId>" + "<version>2.0</version>" + "</dependency>"
+                + "</dependencies>");
         // @formatter:on
-        FileUtils.copyFile(new File(resourcesRootPath + "input/JavaClass.java"),
-            project.getUnderlyingResource().getLocation().append("src/main/java/main/JavaClass.java").toFile());
+        FileUtils.copyFile(new File(resourcesRootPath + "input/JavaClass.java"), project
+            .getUnderlyingResource().getLocation().append("src/main/java/main/JavaClass.java").toFile());
         project.getProject().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
         tmpMavenProjectRule.updateProject();
-
-        // import the configuration project for this test
-        EclipseUtils.importExistingGeneralProject(bot, new File(resourcesRootPath + "templates").getAbsolutePath());
-        EclipseUtils.updateMavenProject(bot, ResourceConstants.CONFIG_PROJECT_NAME);
 
         // expand the new file in the package explorer
         SWTBotView view = bot.viewById(JavaUI.ID_PACKAGES);
@@ -107,4 +116,34 @@ public class CobigenJavaWrapperTest {
             .isEqualTo("@javax.ws.rs.Path(value=/PATH) @javax.ws.rs.Path(value=/PATH)");
     }
 
+    /**
+     * Test for external projects (not in workspace) taken as input for generation
+     * @throws Exception
+     *             test fails
+     */
+    @Test
+    public void testWorkspaceExternalProjectAsInput() throws Exception {
+
+        // copy sample project to external location and import it into the workspace
+        File tmpFolder = tmpFolderRule.newFolder();
+        String testProjName = "ExtTestProj";
+        FileUtils.copyDirectory(new File(resourcesRootPath + "input/" + testProjName), tmpFolder);
+        EclipseUtils.importExistingGeneralProject(bot, tmpFolder.getAbsolutePath(), false);
+
+        // expand the new file in the package explorer
+        SWTBotView view = bot.viewById(JavaUI.ID_PACKAGES);
+        SWTBotTreeItem javaClassItem =
+            view.bot().tree().expandNode(testProjName, "src", "main", "SimpleInput.java");
+        javaClassItem.select();
+
+        // execute CobiGen
+        EclipseCobiGenUtils.processCobiGen(bot, javaClassItem, "increment2");
+        EclipseCobiGenUtils.confirmSuccessfullGeneration(bot);
+
+        // check assertions
+        bot.waitUntil(new AllJobsAreFinished(), 10000);
+        IProject proj = ResourcesPlugin.getWorkspace().getRoot().getProject(testProjName);
+        IFile generationResult = proj.getFile("TestOutput2.txt");
+        assertThat(IOUtils.toString(generationResult.getContents())).isEqualTo("This is a test");
+    }
 }
