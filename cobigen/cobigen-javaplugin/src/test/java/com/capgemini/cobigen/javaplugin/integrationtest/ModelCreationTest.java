84b99faf6250a24932743f7270222bab84431158
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2016-11-16 08:53:20 +0100

    #251 fixed parsed strings as values of annotations to not be enclosed in parenthesis

--- cobigen/cobigen-javaplugin/src/test/java/com/capgemini/cobigen/javaplugin/integrationtest/ModelCreationTest.java
+++ cobigen/cobigen-javaplugin/src/test/java/com/capgemini/cobigen/javaplugin/integrationtest/ModelCreationTest.java
@@ -68,4 +68,39 @@ public class ModelCreationTest extends AbstractIntegrationTest {
         }
     }
 
+    /**
+     * Tests that annotation string values for methods are not quoted. See issue #251
+     * @throws Exception
+     *             test fails
+     */
+    @Test
+    public void testCorrectAnnotationValueExtraction() throws Exception {
+        CobiGen cobiGen = CobiGenFactory.create(cobigenConfigFolder.toURI());
+        File tmpFolderCobiGen = tmpFolder.newFolder("cobigen_output");
+
+        Object[] input = new Object[] { this.getClass(),
+            JavaParserUtil.getFirstJavaClass(getClass().getClassLoader(), new FileReader(new File(
+                "src/test/resources/testdata/integrationtest/javaSources/ModelCreationTest.java"))) };
+        List<TemplateTo> templates = cobiGen.getMatchingTemplates(input);
+
+        boolean methodTemplateFound = false;
+        for (TemplateTo template : templates) {
+            if (template.getId().equals("correctAnnotationValueExtraction.txt")) {
+                GenerationReportTo report =
+                    cobiGen.generate(input, template, Paths.get(tmpFolderCobiGen.getAbsolutePath()), false);
+                assertThat(report).isSuccessful();
+                File expectedFile = new File(tmpFolderCobiGen.getAbsoluteFile() + SystemUtils.FILE_SEPARATOR
+                    + "correctAnnotationValueExtraction.txt");
+                assertThat(expectedFile).exists();
+                assertThat(expectedFile).hasContent("/foo/{id}/");
+                methodTemplateFound = true;
+                break;
+            }
+        }
+
+        if (!methodTemplateFound) {
+            throw new AssertionFailedError("Test template not found");
+        }
+    }
+
 }
