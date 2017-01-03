91212fdc36a03f0e36f3a8f1e120892ae24380ae
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2017-01-03 23:21:47 +0100

    Fix JavaDoc issues for Java8 compatibility

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java-templates/com/capgemini/cobigen/impl/config/constant/MavenMetadata.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java-templates/com/capgemini/cobigen/impl/config/constant/MavenMetadata.java
@@ -3,8 +3,7 @@ package com.capgemini.cobigen.impl.config.constant;
 /**
  * Constants extracted from the maven pom by templating-maven-plugin.
  * 
- * @Generated
- * @see edit template in src/main/java-templates
+ * @see "edit template in src/main/java-templates"
  */
 @SuppressWarnings("javadoc")
 public class MavenMetadata {
