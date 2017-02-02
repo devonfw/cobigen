d259c820637f3da2ff348ba2ebfffbb230ba840c
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-02-02 12:35:58 +0100

    added javadocs

--- cobigen/cobigen-htmlplugin/src/main/java/com/capgemini/cobigen/htmlplugin/merger/ng2/utils/constants/ConstantsNG2.java
+++ cobigen/cobigen-htmlplugin/src/main/java/com/capgemini/cobigen/htmlplugin/merger/ng2/utils/constants/ConstantsNG2.java
@@ -1,5 +1,7 @@
 package com.capgemini.cobigen.htmlplugin.merger.ng2.utils.constants;
 
+import org.jsoup.nodes.Element;
+
 /**
  *
  */
@@ -10,14 +12,34 @@ public class ConstantsNG2 {
      */
     public static final String MD_NAV_LIST = "md-nav-list";
 
+    /**
+     * Referring link tags
+     */
     public static final String A_REF = "a";
 
+    /**
+     * Referring forms tags
+     */
     public static final String FORM = "form";
 
+    /**
+     * referring input containers for forms
+     */
     public static final String INPUT_CONTAINER = "md-input-container";
 
+    /**
+     * Referring input tag for input containers
+     */
     public static final String INPUT = "input";
 
+    /**
+     * Referring name attribute of {@link Element}
+     */
     public static final String NAME_ATTR = "name";
+
+    /**
+     * Referring the *ngIf attribute
+     */
+    public static final String NGIF_ATRR = "*ngIf";
 
 }
