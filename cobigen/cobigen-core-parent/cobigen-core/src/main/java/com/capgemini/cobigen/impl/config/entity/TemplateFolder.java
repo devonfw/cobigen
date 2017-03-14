021871d771d9b89c66e4f4fbb8f927b6e0b20ed4
Joerg Hohwiller <hohwille@users.sourceforge.net>
2017-03-14 17:37:34 +0100

    devonfw/tools-cobigen#157 refactored to work as before

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/capgemini/cobigen/impl/config/entity/TemplateFolder.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/capgemini/cobigen/impl/config/entity/TemplateFolder.java
@@ -1,53 +1,54 @@
 package com.capgemini.cobigen.impl.config.entity;
 
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.Charset;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import com.capgemini.cobigen.api.exception.CobiGenRuntimeException;
+import com.capgemini.cobigen.impl.util.CobiGenPropertiesUtil;
 
 /**
  * Virtual file system for generation target to evaluate path variables and cobigen specific symlinks.
  */
-public class TemplateFolder {
+public class TemplateFolder extends TemplatePath {
 
-    private static final String COBIGEN_PROPERTIES = "cobigen.properties";
-
-    private static final Charset UTF_8 = Charset.forName("UTF-8");
-
-    /** Parent folder of the virtual file system. */
-    private final TemplateFolder parent;
-
-    /** @see #getTemplatePath() */
-    private final Path templatePath;
-
-    /** @see #getTargetPath() */
-    private final Path targetPath;
-
-    /** Children mapped from name to {@link TemplateFolder} instance - see {@link #getChild(String)}. */
-    private final Map<String, TemplateFolder> children;
+    /** Children mapped from name to {@link TemplatePath} instance - see {@link #getChild(String)}. */
+    private final Map<String, TemplatePath> children;
 
     /** @see #getVariables() */
     private final Properties variables;
 
+    /** @see #getChildFiles() */
+    private List<TemplateFile> childFiles;
+
+    /** @see #getChildFolders() */
+    private List<TemplateFolder> childFolders;
+
+    /** {@code true} if children have been {@link #scanChildren() scanned}, {@code false} otherwise. */
+    private boolean childrenScanned;
+
     /**
      * Constructor for root folder.
      *
-     * @param path
-     *            see {@link #getResolvedAbsolutePath()}.
+     * @param templatePath
+     *            the {@link #getPath() template path}.
+     * @param variables
+     *            the top-level {@link Properties} to inherit.
      */
-    private TemplateFolder(Path path) {
-        super();
-        parent = null;
-        templatePath = path;
-        targetPath = templatePath; // TODO
+    private TemplateFolder(Path templatePath, Properties variables) {
+        super(templatePath, null);
         children = new HashMap<>();
-        variables = readLocalProperties(path, new Properties());
+        this.variables = CobiGenPropertiesUtil.load(templatePath, variables);
     }
 
     /**
@@ -59,44 +60,43 @@ public class TemplateFolder {
      *            the name of this child folder to create.
      */
     private TemplateFolder(TemplateFolder parent, String name) {
-        super();
-        this.parent = parent;
-        templatePath = this.parent.templatePath.resolve(name);
-        targetPath = templatePath; // TODO
+        this(parent.getPath().resolve(name), parent);
+    }
+
+    /**
+     * Constructor for child folder.
+     *
+     * @param templatePath
+     *            the {@link #getPath() template path}.
+     * @param parent
+     *            the {@link #getParent() parent folder}.
+     */
+    private TemplateFolder(Path templatePath, TemplateFolder parent) {
+        super(templatePath, parent);
         children = new HashMap<>();
-        variables = readLocalProperties(templatePath, this.parent.variables);
+        variables = CobiGenPropertiesUtil.load(templatePath, parent.variables);
     }
+    //
+    // /**
+    // *
+    // *
+    // *
+    // * @return the unresolved path relative to {@code basedir} (the top-level folder of the source project
+    // of
+    // * the generation containing the input file). Examples:
+    // * <ul>
+    // * <li>"java/${rootPackage}/${component}/common/api/${detail}"</li>
+    // * <li>"../api/${destinationPath}/${rootPackage}/${component}/common/api/${detail}"</li>
+    // * <li>""</li>
+    // * </ul>
+    // */
+    // public String getTargetPath() {
+    // return targetPath;
+    // }
 
     /**
-     * @return the parent {@link TemplateFolder} or <code>null</code> if this is the root (top-level folder of
-     *         the generation input source).
-     */
-    public TemplateFolder getParent() {
-        return parent;
-    }
-
-    /**
-     * Returns the resolved absolute {@link Path} for this {@link TemplateFolder} instance.
-     * @return the resolved absolute {@link Path}
-     */
-    public Path getTemplatePath() {
-        return templatePath;
-    }
-
-    /**
-     *
-     * ../api/src/main/java/.../${component}/../${pojo.name}Cto.java
-     *
-     * Returns the resolved absolute {@link Path} for this {@link TemplateFolder} instance.
-     * @return the resolved absolute {@link Path}
-     */
-    public Path getTargetPath() {
-        return targetPath;
-    }
-
-    /**
-     * @return the {@link Map} with the variables given when the VFS was {@link VfsFactory#create(Path, Map)
-     *         created} merged with potential variables from {@link Properties property-files}
+     * @return the {@link Map} with the variables given when the VFS was {@link #create(Path) created} merged
+     *         with potential variables from {@link Properties property-files}
      */
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public Map<String, String> getVariables() {
@@ -104,49 +104,154 @@ public class TemplateFolder {
     }
 
     /**
-     * Returns the child with the given name or creates a new one if not already existing.
      * @param name
-     *            of the child
-     * @return the child with the given name.
+     *            the filename of the child in this folder.
+     * @return the requested child or {@code null} if no such file exists.
      */
-    public TemplateFolder getChild(String name) {
-        TemplateFolder child = children.get(name);
+    public TemplatePath getChild(String name) {
+        TemplatePath child = children.get(name);
         if (child == null) {
-            child = new TemplateFolder(this, name);
-            children.put(name, child);
+            Path childPath = getPath().resolve(name);
+            child = createChild(childPath);
+            if (child != null) {
+                children.put(name, child);
+            }
         }
         return child;
     }
 
-    private static final Properties readLocalProperties(Path folder, Properties parent) {
+    /**
+     * @return the {@link Collection} with all {@link #getChild(String) child} {@link TemplatePath#isFile()
+     *         files}.
+     */
+    public Collection<TemplatePath> getChildren() {
 
-        Path propertiesPath = folder.resolve(COBIGEN_PROPERTIES);
-        if (!Files.exists(propertiesPath)) {
-            return parent;
+        scanChildren();
+        return children.values();
+    }
+
+    /**
+     * @return the {@link Collection} with all {@link #getChild(String) child} {@link TemplatePath#isFile()
+     *         files}.
+     */
+    public Collection<TemplateFile> getChildFiles() {
+
+        scanChildren();
+        return childFiles;
+    }
+
+    /**
+     * @return the {@link Collection} with all {@link #getChild(String) child} {@link TemplatePath#isFolder()
+     *         folders}.
+     */
+    public Collection<TemplateFolder> getChildFolders() {
+
+        scanChildren();
+        return childFolders;
+    }
+
+    /**
+     * Scans and creates all children.
+     */
+    private void scanChildren() {
+        if (childrenScanned) {
+            return;
         }
-
-        Properties properties = new Properties(parent);
-        try (Reader reader = Files.newBufferedReader(propertiesPath, UTF_8)) {
-            properties.load(reader);
+        Path templatePath = getPath();
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(templatePath)) {
+            for (Path childName : directoryStream) {
+                String filename = childName.getFileName().toString();
+                if (!CobiGenPropertiesUtil.COBIGEN_PROPERTIES.equals(filename)) {
+                    TemplatePath child = createChild(childName);
+                    children.put(filename, child);
+                }
+            }
+            if (childFiles == null) {
+                childFiles = Collections.emptyList();
+            } else {
+                childFiles = Collections.unmodifiableList(childFiles);
+            }
+            if (childFolders == null) {
+                childFolders = Collections.emptyList();
+            } else {
+                childFolders = Collections.unmodifiableList(childFolders);
+            }
+            childrenScanned = true;
         } catch (IOException e) {
-            throw new CobiGenRuntimeException("Failed to read " + COBIGEN_PROPERTIES + " from " + folder, e);
+            throw new CobiGenRuntimeException("Failed to list directory of " + templatePath, e);
         }
-        return properties;
+    }
+
+    /**
+     * @param childPath
+     *            the {@link Path} pointing to the child.
+     * @return the new {@link TemplatePath} representing the child.
+     */
+    private TemplatePath createChild(Path childPath) {
+        if (Files.isDirectory(childPath)) {
+            TemplateFolder templateFolder = new TemplateFolder(childPath, this);
+            if (childFolders == null) {
+                childFolders = new ArrayList<>();
+            }
+            childFolders.add(templateFolder);
+            return templateFolder;
+        } else if (Files.exists(childPath)) {
+            TemplateFile templateFile = new TemplateFile(childPath, this);
+            if (childFiles == null) {
+                childFiles = new ArrayList<>();
+            }
+            childFiles.add(templateFile);
+            return templateFile;
+        } else {
+            return null;
+        }
     }
 
     /**
      * Navigates to the given sub path. Slash is considered as path delimiter.
-     * @param subpath
+     * @param relativePath
      *            relative sub path to navigate to.
      * @return the {@link TemplateFolder} instance representing the sub path.
      */
-    public TemplateFolder navigate(String subpath) {
-        return null;
+    public TemplatePath navigate(String relativePath) {
+        TemplateFolder folder = this;
+        Iterator<Path> it = Paths.get(relativePath).iterator();
+        while (it.hasNext()) {
+            String filename = it.next().toString();
+            if (filename.equals("..")) {
+                TemplateFolder parent = folder.getParent();
+                if (parent != null) {
+                    folder = parent;
+                }
+            } else if (!filename.equals(".")) {
+                TemplatePath child = folder.getChild(filename);
+                if (it.hasNext()) {
+                    if ((child == null) || child.isFile()) {
+                        return null;
+                    }
+                    folder = (TemplateFolder) child;
+                } else {
+                    return child;
+                }
+            }
+        }
+        return folder;
     }
 
+    /**
+     * @param rootPath
+     *            the root {@link Path} containing the templates (typically where {@code templates.xml} is
+     *            located).
+     * @return a new {@link TemplateFolder} instance for the given {@link Path}.
+     */
     public static TemplateFolder create(Path rootPath) {
 
-        return new TemplateFolder(rootPath);
+        return new TemplateFolder(rootPath, new Properties());
+    }
+
+    @Override
+    public boolean isFolder() {
+        return true;
     }
 
 }
