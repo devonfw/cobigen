58fb3cd52cca3c1cd923eb7d648552595feea80e
Marian Müller <marian.mueller@capgemini.com>
2017-04-10 13:26:11 +0200

    wrote testcases for issues #290 and #291

--- cobigen/cobigen-javaplugin/src/test/java/com/capgemini/cobigen/javaplugin/unittest/merger/libextension/CustomModelWriterTest.java
+++ cobigen/cobigen-javaplugin/src/test/java/com/capgemini/cobigen/javaplugin/unittest/merger/libextension/CustomModelWriterTest.java
@@ -132,4 +132,42 @@ public class CustomModelWriterTest {
         String reprintedClass = target.toString();
         assertThat(reprintedClass).contains("@Multipart(value=\"binaryObjectEto\"");
     }
+
+    /**
+     * Tests the output of the CustomModelWriter with respect to the syntax of the array notation for
+     * annotation parameter values.
+     *
+     * See https://github.com/devonfw/tools-cobigen/issues/290
+     * @throws Exception
+     *             test fails
+     */
+    @Test
+    public void testCorrectSyntaxOutputForArrays() throws Exception {
+        File file = new File(testFileRootPath + "ArraySyntax.java");
+        CustomModelWriter target = new CustomModelWriter();
+        JavaClass parsedClass = JavaParserUtil.getFirstJavaClass(new FileReader(file));
+        target.writeClass(parsedClass);
+
+        String reprintedClass = target.toString();
+        assertThat(reprintedClass).containsSequence("{", "{", "}", "{", "}", "}");
+    }
+
+    /**
+     * Tests the output of the CustomModelWriter regarding the full qualified names of the nested annotations
+     * used in it.
+     *
+     * See https://github.com/devonfw/tools-cobigen/issues/291
+     * @throws Exception
+     *             test fails
+     */
+    @Test
+    public void testCorrectNameOutputForNestedAnnotations() throws Exception {
+        File file = new File(testFileRootPath + "ArraySyntax.java");
+        CustomModelWriter target = new CustomModelWriter();
+        JavaClass parsedClass = JavaParserUtil.getFirstJavaClass(new FileReader(file));
+        target.writeClass(parsedClass);
+
+        String reprintedClass = target.toString();
+        assertThat(reprintedClass).doesNotContain("javax.persistence.");
+    }
 }
