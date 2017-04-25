5af17d0c5a7686802d294bc02b1984457a71fb5a
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2017-04-25 13:29:58 +0200

    #308 maven test implemented for entity input

--- cobigen-maven/cobigen-maven-systemtest/src/test/java/com/capgemini/cobigen/maven/systemtest/Oasp4JTemplateTest.java
+++ cobigen-maven/cobigen-maven-systemtest/src/test/java/com/capgemini/cobigen/maven/systemtest/Oasp4JTemplateTest.java
@@ -3,6 +3,7 @@ package com.capgemini.cobigen.maven.systemtest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -21,12 +22,22 @@ import com.capgemini.cobigen.maven.config.constant.MavenMetadata;
 /** Test suite for testing the maven plugin with whole released template sets */
 public class Oasp4JTemplateTest {
 
+    /** Root of all test resources of this test suite */
+    public static final String TEST_RESOURCES_ROOT = "src/test/resources/testdata/systemtest/Oasp4JTemplateTest/";
+
+    /** Temporary folder rule to create new temporary folder and files */
     @Rule
     public TemporaryFolder tmpFolder = new TemporaryFolder();
 
+    /**
+     * Processes a generation of oasp4j template increments daos and entity_infrastructure and just checks
+     * whether the files have been generated.
+     * @throws Exception
+     *             test fails
+     */
     @Test
-    public void testEntityInputDataaccessGeneneration() throws Exception {
-        File testProject = new File("src/test/resources/testdata/systemtest/Oasp4JTemplateTest/");
+    public void testEntityInputDataaccessGeneration() throws Exception {
+        File testProject = new File(TEST_RESOURCES_ROOT + "TestEntityInputDataaccessGeneration/");
         assertThat(testProject).exists();
 
         File testProjectRoot = tmpFolder.newFolder();
@@ -45,6 +56,12 @@ public class Oasp4JTemplateTest {
 
         assertThat(result.getExecutionException()).isNull();
         assertThat(result.getExitCode()).as("Exit Code").isEqualTo(0);
+
+        assertThat(testProjectRoot.list()).containsExactly("pom.xml", "src", "target");
+        long numFilesInTarget =
+            Files.walk(testProjectRoot.toPath().resolve("src")).filter(Files::isRegularFile).count();
+        // 3 from entity_infrastructure, 4 from daos, 1 input file
+        assertThat(numFilesInTarget).isEqualTo(8);
 
     }
 
