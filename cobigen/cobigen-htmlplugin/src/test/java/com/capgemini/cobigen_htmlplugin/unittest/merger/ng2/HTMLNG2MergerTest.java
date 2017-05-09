dacdd5aa73b6503e755b51ceef77f8cb1be469f4
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-05-09 10:29:31 +0200

    renamed HTMLMerger to AngularMerger, removed Angular2Merger

--- cobigen/cobigen-htmlplugin/src/test/java/com/capgemini/cobigen_htmlplugin/unittest/merger/ng2/HTMLNG2MergerTest.java
+++ cobigen/cobigen-htmlplugin/src/test/java/com/capgemini/cobigen_htmlplugin/unittest/merger/ng2/HTMLNG2MergerTest.java
@@ -16,8 +16,8 @@ import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import com.capgemini.cobigen.api.exception.MergeException;
-import com.capgemini.cobigen.htmlplugin.merger.HTMLMerger;
-import com.capgemini.cobigen.htmlplugin.merger.ng2.utils.constants.ConstantsNG2;
+import com.capgemini.cobigen.htmlplugin.merger.AngularMerger;
+import com.capgemini.cobigen.htmlplugin.merger.constants.Constants;
 
 @SuppressWarnings("javadoc")
 public class HTMLNG2MergerTest {
@@ -27,47 +27,47 @@ public class HTMLNG2MergerTest {
     @Test
     public void htmlMergeTest_AddSideBarButton() {
         Document mergedContents =
-            htmlMerger(testFileRootPathNG2, "app.component.html_Base", "app.component.html_Patch", false, "htmlng2");
-        Element sideBar = mergedContents.getElementsByTag(ConstantsNG2.MD_NAV_LIST).first();
-        Elements listEntry = sideBar.getElementsByTag(ConstantsNG2.A_REF);
+            htmlMerger(testFileRootPathNG2, "app.component.html_Base", "app.component.html_Patch", false, "html-ng*");
+        Element sideBar = mergedContents.getElementsByTag(Constants.MD_NAV_LIST).first();
+        Elements listEntry = sideBar.getElementsByTag(Constants.A_REF);
         assertTrue(listEntry.size() == 3);
     }
 
     @Test
     public void htmlMergeTest_AddSideBarButton_Override() {
         Document mergedContents = htmlMerger(testFileRootPathNG2, "app.component.html_Base", "app.component.html_Patch",
-            true, "htmlng2_override");
-        Element sideBar = mergedContents.getElementsByTag(ConstantsNG2.MD_NAV_LIST).first();
-        Elements listEntry = sideBar.getElementsByTag(ConstantsNG2.A_REF);
+            true, "html-ng*_override");
+        Element sideBar = mergedContents.getElementsByTag(Constants.MD_NAV_LIST).first();
+        Elements listEntry = sideBar.getElementsByTag(Constants.A_REF);
         assertTrue(listEntry.size() == 2);
     }
 
     @Test
     public void htmlMergeTest_AddFilterField() {
         Document mergedContents = htmlMerger(testFileRootPathNG2, "dataGrid.component.html_Base",
-            "dataGrid.component.html_Patch", false, "htmlng2");
-        assertTrue(mergedContents.getElementsByTag(ConstantsNG2.INPUT_CONTAINER).size() == 5);
+            "dataGrid.component.html_Patch", false, "html-ng*");
+        assertTrue(mergedContents.getElementsByTag(Constants.INPUT_CONTAINER).size() == 5);
     }
 
     @Test
     public void htmlMergeTest_AddFilterField_Override() {
         Document mergedContents = htmlMerger(testFileRootPathNG2, "dataGrid.component.html_Base",
-            "dataGrid.component.html_Patch", true, "htmlng2_override");
-        assertTrue(mergedContents.getElementsByTag(ConstantsNG2.INPUT_CONTAINER).size() == 1);
+            "dataGrid.component.html_Patch", true, "html-ng*_override");
+        assertTrue(mergedContents.getElementsByTag(Constants.INPUT_CONTAINER).size() == 1);
     }
 
     @Test
     public void htmlMergeTest_AddDialogAddField() {
         Document mergedContents = htmlMerger(testFileRootPathNG2, "addDialog.component.html_Base",
-            "addDialog.component.html_Patch", false, "htmlng2");
-        assertTrue(mergedContents.getElementsByTag(ConstantsNG2.INPUT_CONTAINER).size() == 5);
+            "addDialog.component.html_Patch", false, "html-ng*");
+        assertTrue(mergedContents.getElementsByTag(Constants.INPUT_CONTAINER).size() == 5);
     }
 
     @Test
     public void htmlMergeTest_AddDialogAddField_Override() {
         Document mergedContents = htmlMerger(testFileRootPathNG2, "addDialog.component.html_Base",
-            "addDialog.component.html_Patch", true, "htmlng2_override");
-        assertTrue(mergedContents.getElementsByTag(ConstantsNG2.INPUT_CONTAINER).size() == 1);
+            "addDialog.component.html_Patch", true, "html-ng*_override");
+        assertTrue(mergedContents.getElementsByTag(Constants.INPUT_CONTAINER).size() == 1);
     }
 
     public Document htmlMerger(String rootPath, String fileBase, String filePatch, boolean patchOverrides,
@@ -88,7 +88,7 @@ public class HTMLNG2MergerTest {
         } catch (IOException e) {
             throw new MergeException(htmlPatchFile, "Can not read the base file " + htmlPatchFile.getAbsolutePath());
         }
-        return Jsoup.parse(new HTMLMerger(mergeStrategy, patchOverrides).merge(htmlBaseFile, patchString, "UTF-8"),
+        return Jsoup.parse(new AngularMerger(mergeStrategy, patchOverrides).merge(htmlBaseFile, patchString, "UTF-8"),
             "UTF-8");
 
     }
