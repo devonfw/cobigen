f231346ee0f1d9940f1c23e73f8cc1c0b685136e
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-05-25 16:26:48 +0200

    node command to ts-node command

--- cobigen/cobigen-tsplugin/src/main/java/com/capgemini/cobigen/tsplugin/merger/TypeScriptMerger.java
+++ cobigen/cobigen-tsplugin/src/main/java/com/capgemini/cobigen/tsplugin/merger/TypeScriptMerger.java
@@ -106,7 +106,7 @@ public class TypeScriptMerger implements Merger {
             unzipper.unzip(zipFile, "/tmp/tsm");
         }
 
-        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "node \\tmp\\tsm\\src\\index.js " + patchOverrides
+        ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", "ts-node \\tmp\\tsm\\src\\index.ts " + patchOverrides
             + " " + base.getAbsolutePath() + " " + outPatch.getAbsolutePath());
 
         builder.redirectErrorStream(true);
