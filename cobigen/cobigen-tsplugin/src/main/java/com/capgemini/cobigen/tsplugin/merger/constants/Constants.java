25aa079abc8a18cb59afa923313291ba04540b56
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-06-14 18:23:56 +0200

    added tsplugin with clean history

---
+++ cobigen/cobigen-tsplugin/src/main/java/com/capgemini/cobigen/tsplugin/merger/constants/Constants.java
@@ -0,0 +1,34 @@
+package com.capgemini.cobigen.tsplugin.merger.constants;
+
+import com.capgemini.cobigen.tsplugin.merger.TypeScriptMerger;
+
+/**
+ * List of constants used on the {@link TypeScriptMerger}
+ */
+public class Constants {
+
+    /**
+     * Path for the JS Beautifier script
+     */
+    public static final String BEAUTIFY_JS = "/beautify.js";
+
+    /**
+     * Path for the TSMerger
+     */
+    public static final String TS_MERGER = "/tsmerger.js";
+
+    /**
+     * Temporary folder name
+     */
+    public static final String COBIGEN_TS = "cobigen-ts";
+
+    /**
+     * Temporary patch file
+     */
+    public static final String PATCH_TS = "patch.ts";
+
+    /**
+     * Temporary TS Merger JS file
+     */
+    public static final String TSMERGER_JS = "tsmerger.js";
+}
