6746de8966ae369dfe0a9bf76b1935ba87f9c909
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2017-06-21 09:03:55 +0200

    #327 provide more test robustness

--- cobigen-eclipse/cobigen-eclipse-test/src/main/java/com/capgemini/cobigen/eclipse/test/common/utils/EclipseUtils.java
+++ cobigen-eclipse/cobigen-eclipse-test/src/main/java/com/capgemini/cobigen/eclipse/test/common/utils/EclipseUtils.java
@@ -86,6 +86,8 @@ public class EclipseUtils {
         bot.waitUntil(widgetIsEnabled(finishButton));
         finishButton.click();
         bot.waitUntil(new AllJobsAreFinished(), 20000);
+        ResourcesPlugin.getWorkspace().getRoot().refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+        bot.waitUntil(new AllJobsAreFinished(), 20000);
         ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, new NullProgressMonitor());
         bot.waitUntil(new AllJobsAreFinished(), 20000);
     }
