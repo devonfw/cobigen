920369c7ed34b2786d5a1f49d6b2dd47f0efa181
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2017-08-01 09:42:07 +0200

    #19 adapt to have no more dependencies on cobigen-core

--- cobigen/cobigen-senchaplugin/src/test/java/com/capgemini/cobigen/senchaplugin/integrationtest/SenchaIntegrationTest.java
+++ cobigen/cobigen-senchaplugin/src/test/java/com/capgemini/cobigen/senchaplugin/integrationtest/SenchaIntegrationTest.java
@@ -4,12 +4,9 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
 
-import junit.framework.AssertionFailedError;
-
 import org.apache.commons.io.Charsets;
 import org.apache.commons.lang3.SystemUtils;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -17,12 +14,9 @@ import org.junit.rules.TemporaryFolder;
 import com.capgemini.cobigen.api.CobiGen;
 import com.capgemini.cobigen.api.to.TemplateTo;
 import com.capgemini.cobigen.impl.CobiGenFactory;
-import com.capgemini.cobigen.impl.PluginRegistry;
-import com.capgemini.cobigen.impl.TemplateEngineRegistry;
-import com.capgemini.cobigen.javaplugin.JavaPluginActivator;
-import com.capgemini.cobigen.senchaplugin.SenchaPluginActivator;
 import com.capgemini.cobigen.senchaplugin.integrationtest.testdata.ModelCreationTest;
-import com.capgemini.cobigen.tempeng.freemarker.FreeMarkerTemplateEngine;
+
+import junit.framework.AssertionFailedError;
 
 @SuppressWarnings("javadoc")
 public class SenchaIntegrationTest {
@@ -38,30 +32,16 @@ public class SenchaIntegrationTest {
     @Rule
     public TemporaryFolder tmpFolder = new TemporaryFolder();
 
-    /**
-     * Common test setup
-     */
-    @Before
-    public void setup() {
-        PluginRegistry.loadPlugin(SenchaPluginActivator.class);
-        TemplateEngineRegistry.register(FreeMarkerTemplateEngine.class);
-    }
-
     @Test
     public void testCorrectModelGeneration() throws Exception {
-
-        PluginRegistry.loadPlugin(SenchaPluginActivator.class);
-        PluginRegistry.loadPlugin(JavaPluginActivator.class);
 
         CobiGen cobiGen = CobiGenFactory.create(cobigenConfigFolder.toURI());
         File tmpFolderCobiGen = tmpFolder.newFolder("cobigen_output");
 
-        Object[] input =
-            new Object[] {
-                ModelCreationTest.class,
-                cobiGen.read("java",
-                    Paths.get("src/test/resources/testdata/integrationtest/javaSources/ModelCreationTest.java"),
-                    Charsets.UTF_8) };
+        Object[] input = new Object[] { ModelCreationTest.class,
+            cobiGen.read("java",
+                Paths.get("src/test/resources/testdata/integrationtest/javaSources/ModelCreationTest.java"),
+                Charsets.UTF_8) };
         List<TemplateTo> templates = cobiGen.getMatchingTemplates(input);
 
         boolean methodTemplateFound = false;
