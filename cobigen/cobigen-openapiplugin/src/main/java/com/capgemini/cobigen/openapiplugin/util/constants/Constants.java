72c3d2477a83bdde7f3c09fc357c2d50cb671f15
Ruben Diaz <ruben.diaz-martinez@capgemini.com>
2017-10-19 18:56:51 +0200

    added relationship definition support

--- cobigen/cobigen-openapiplugin/src/main/java/com/capgemini/cobigen/openapiplugin/util/constants/Constants.java
+++ cobigen/cobigen-openapiplugin/src/main/java/com/capgemini/cobigen/openapiplugin/util/constants/Constants.java
@@ -14,4 +14,14 @@ public class Constants {
 
     public static final String SEARCH_CRITERIA = "searchCriteria";
 
+    public static final String COMPONENT_EXT = "x-component";
+
+    public static final String ONE_TO_ONE = "onetoone";
+
+    public static final String MANY_TO_ONE = "manytoone";
+
+    public static final String ONE_TO_MANY = "onetomany";
+
+    public static final String MANY_TO_MANY = "manytomany";
+
 }
