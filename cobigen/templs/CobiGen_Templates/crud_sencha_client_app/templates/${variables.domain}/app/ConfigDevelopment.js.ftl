4ffedd6bd1db0c38af20acfa6482528f5bcb126b
Diaz Gonzalez <jaime.diaz-gonzalez-external@capgemini.com>
2017-10-27 10:52:42 +0200

    Adding templates folder

---
+++ cobigen/templs/CobiGen_Templates/crud_sencha_client_app/templates/${variables.domain}/app/ConfigDevelopment.js.ftl
@@ -0,0 +1,2 @@
+window.Config.server = 'http://localhost:8081/${variables.rootPackage}-server/services/rest/';
+window.Config.CORSEnabled = true;
---
+++ cobigen/templs/CobiGen_Test/src/main/java/umlTest/Bill.java
@@ -0,0 +1,12 @@
+package main.java.umlTest;
+
+// EA_Model
+// Logical View
+public class Bill {
+    // CLASS NAME: Bill
+
+    public int x;
+
+    public String y;
+
+}
