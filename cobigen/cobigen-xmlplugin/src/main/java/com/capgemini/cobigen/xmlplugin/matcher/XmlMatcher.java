2ad43e92b59ddae1d09f6ab822f8eaea11f304bc
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2017-11-08 13:19:57 +0100

    #112 #65 #443 enabled namespace aware xpath expressions for matchers. refactored input reader to return all nodes in a generic way in case of treated as a container.

--- cobigen/cobigen-xmlplugin/src/main/java/com/capgemini/cobigen/xmlplugin/matcher/XmlMatcher.java
+++ cobigen/cobigen-xmlplugin/src/main/java/com/capgemini/cobigen/xmlplugin/matcher/XmlMatcher.java
@@ -4,52 +4,41 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
+import com.capgemini.cobigen.api.exception.CobiGenRuntimeException;
 import com.capgemini.cobigen.api.exception.InvalidConfigurationException;
 import com.capgemini.cobigen.api.extension.MatcherInterpreter;
 import com.capgemini.cobigen.api.to.MatcherTo;
 import com.capgemini.cobigen.api.to.VariableAssignmentTo;
-import com.google.common.collect.Maps;
+import com.capgemini.cobigen.xmlplugin.inputreader.XmlInputReader;
 
-/**
- * {@link MatcherInterpreter} for XML matcher configurations.
- */
+/** {@link MatcherInterpreter} for XML matcher configurations. */
 public class XmlMatcher implements MatcherInterpreter {
 
-    /**
-     * Assigning logger to XmlClassMatcher
-     */
+    /** Assigning logger to XmlClassMatcher */
     private static final Logger LOG = LoggerFactory.getLogger(XmlMatcher.class);
 
-    private XPathLogic logic = new XPathLogic();
-
-    /**
-     * Currently supported matcher types
-     *
-     * @author fkreis (18.11.2014)
-     */
+    /** Currently supported matcher types */
     private enum MatcherType {
         /** Document's root name */
         NODENAME,
         /** Should match if input is a Document */
-        DOCUMENT,
-        /** Container matcher for UML diagrams */
-        UML
+        XPATH
     }
 
-    /**
-     * Available variable types for the matcher
-     *
-     * @author fkreis (18.11.2014)
-     */
+    /** Available variable types for the matcher */
     private enum VariableType {
         /** Constant variable assignment */
         CONSTANT,
-        /** Regular expression group assignment */
-        REGEX,
         /** Xpath expression group assignment */
         XPATH
     }
@@ -68,20 +57,58 @@ public class XmlMatcher implements MatcherInterpreter {
                         && matcher.getValue().matches(documentRootName);
                 }
                 break;
-            case DOCUMENT:
-                return matcher.getTarget().getClass().getSimpleName().toLowerCase()
-                    .equals(matcher.getValue().toLowerCase());
-            case UML:
-                if (target instanceof Document) {
-                    return ((Document) target).getDocumentElement().getNodeName().equals(matcher.getValue());
+            case XPATH:
+                Document targetDoc = getDoc(target, 1);
+                XPath xPath = createXpathObject(target);
+                String xpathExpression = matcher.getValue();
+                try {
+                    return ((NodeList) xPath.evaluate(xpathExpression, targetDoc, XPathConstants.NODESET))
+                        .getLength() > 0;
+                } catch (XPathExpressionException e) {
+                    throw new CobiGenRuntimeException("Invalid XPath expression: " + xpathExpression, e);
                 }
-                break;
             }
-
         } catch (IllegalArgumentException e) {
-            LOG.info("Matcher type '{}' not registered --> no match!", matcher.getType());
+            throw new CobiGenRuntimeException("Matcher type " + matcher.getType() + " not registered!", e);
         }
         return false;
+    }
+
+    /**
+     * @param target
+     * @return
+     */
+    private XPath createXpathObject(Object target) {
+        Document fullDoc = getDoc(target, 0);
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        xPath.setNamespaceContext(new NamespaceResolver(fullDoc));
+        return xPath;
+    }
+
+    /**
+     * Returns the document provided in the input object. Either the object is an {@link Document}, or an
+     * {@link Document} array. In the latter case, the document at the preferred index is returned. As of
+     * {@link XmlInputReader#getInputObjects(Object, java.nio.charset.Charset)}, the preferred index 0 points
+     * at the full {@link Document} and the preferred index 1 points at the partial {@link Document} in case
+     * of an array as input.
+     * @param target
+     *            generator input, which has to be a {@link Document} or {@link Document} array dependent on
+     *            the return types of {@link XmlInputReader#getInputObjects(Object, java.nio.charset.Charset)}
+     * @param preferredIndex
+     *            index pointing at the document to be returned in case of {@link Document} array as target
+     * @return the preferred {@link Document}
+     */
+    private Document getDoc(Object target, int preferredIndex) {
+        Document targetDoc;
+        if (target instanceof Document) {
+            targetDoc = (Document) target;
+        } else if (target instanceof Document[]) {
+            targetDoc = ((Document[]) target)[preferredIndex];
+        } else {
+            throw new IllegalArgumentException(
+                "Unknown input object of type " + target.getClass() + " in matcher execution.");
+        }
+        return targetDoc;
     }
 
     @Override
@@ -98,8 +125,11 @@ public class XmlMatcher implements MatcherInterpreter {
                     switch (variableType) {
                     case CONSTANT:
                         resolvedVariables.put(va.getVarName(), va.getValue());
-                    case REGEX:
-                        // TODO #64
+                        break;
+                    case XPATH:
+                        resolvedVariables.put(va.getVarName(),
+                            resolveVariablesXPath(getDoc(matcher.getTarget(), 1), va.getValue()));
+                        break;
                     }
                 }
                 return resolvedVariables;
@@ -111,7 +141,8 @@ public class XmlMatcher implements MatcherInterpreter {
                 VariableType variableType = Enum.valueOf(VariableType.class, va.getType().toUpperCase());
                 switch (variableType) {
                 case XPATH:
-                    resolvedVariables.put(va.getVarName(), logic.resolveVariablesXPath(matcher, va));
+                    resolvedVariables.put(va.getVarName(),
+                        resolveVariablesXPath(getDoc(matcher.getTarget(), 1), va.getValue()));
                     break;
                 case CONSTANT:
                     resolvedVariables.put(va.getVarName(), va.getValue());
@@ -121,9 +152,24 @@ public class XmlMatcher implements MatcherInterpreter {
             }
             return resolvedVariables;
         } catch (IllegalArgumentException e) {
-            LOG.warn("Matcher type '{}' not registered --> no match!", matcher.getType());
+            throw new CobiGenRuntimeException(
+                "Matcher or VariableAssignment type " + matcher.getType() + " not registered!", e);
         }
-        return Maps.newHashMap();
     }
 
+    private String resolveVariablesXPath(Document doc, String xpathExpression) {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        try {
+            NodeList list = (NodeList) xPath.evaluate(xpathExpression, doc, XPathConstants.NODESET);
+            if (list.getLength() > 0) {
+                // currently, we just allow strings as variable assignment values
+                return list.item(0).getTextContent();
+            } else {
+                return ""; // we have to return empty string as of Variables restrictions of cobigen-core
+            }
+        } catch (XPathExpressionException e) {
+            throw new CobiGenRuntimeException("Could not evaluate xpath expression " + xpathExpression, e);
+        }
+    }
 }
---
+++ cobigen/cobigen-xmlplugin/src/test/resources/testdata/integrationtest/uml-classdiag/uml_test/templates/${className}.txt.ftl
@@ -0,0 +1,2 @@
+<#assign name = elemDoc["/packagedElement/@name"]/>
+${packagedElement._at_visibility} ${name} <#-- ${doc["//packagedElement[@name='" + name + "']/@id"]} -->
