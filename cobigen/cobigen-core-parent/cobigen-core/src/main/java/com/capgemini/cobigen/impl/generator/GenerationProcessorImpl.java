e76dc34522e4b5979836c93d09dad0b64ef0c64d
richter-cg <christian.a.richter@capgemini.com>
2018-03-14 12:01:06 +0100

    #497 changed exception definition

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/capgemini/cobigen/impl/generator/GenerationProcessorImpl.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/capgemini/cobigen/impl/generator/GenerationProcessorImpl.java
@@ -347,8 +347,7 @@ public class GenerationProcessorImpl implements GenerationProcessor {
                         if (merger != null) {
                             mergeResult = merger.merge(tmpOriginalFile, patch, targetCharset);
                         } else {
-                            throw new PluginNotAvailableException(trigger.getType(),
-                                "merge strategy '" + templateEty.getMergeStrategy() + "'");
+                            throw new PluginNotAvailableException("merge strategy '" + templateEty.getMergeStrategy() + "'");
                         }
 
                         if (mergeResult != null) {
