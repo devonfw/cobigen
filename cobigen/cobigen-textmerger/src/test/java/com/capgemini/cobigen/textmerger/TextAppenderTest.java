d6f149c67efc53c89855eec9717d033063ea4796
MikeSchumacherCapgemini <mike.schumacher@capgemini.com>
2018-04-20 15:20:47 +0200

    Created Tests to check for multiple merge requirements, added multiple text files that contain text before and after merging, started work on TextAppender, created inner class MergeUtil to take a lot of code out of the merge method. TODO: manage to get comment opening tags into map keys, append parts of the document(s) that aren't header or footer

--- cobigen/cobigen-textmerger/src/test/java/com/capgemini/cobigen/textmerger/TextAppenderTest.java
+++ cobigen/cobigen-textmerger/src/test/java/com/capgemini/cobigen/textmerger/TextAppenderTest.java
@@ -1,10 +1,14 @@
 package com.capgemini.cobigen.textmerger;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.capgemini.cobigen.textmerger.TextAppender.MergeUtil;
 
 /**
  *
@@ -18,9 +22,20 @@ public class TextAppenderTest {
     private static String testFileRootPath = "src/test/resources/";
 
     /**
+     *
+     */
+    private static Map<String, String> toBe = new HashMap<String, String>() {
+
+        {
+            put("// anchor:test:test:anchorend", "\nline1\n");
+            put("// anchor:test2:test2:anchorend", "\nline2\n");
+        }
+    };
+
+    /**
      * Tests a merge without adding a new line before appending the patch
      * @throws Exception
-     *             if errors occured while merging
+     *             if errors occur while merging
      * @author mbrunnli (03.06.2014)
      */
     @Test
@@ -33,7 +48,7 @@ public class TextAppenderTest {
     /**
      * Tests a merge with adding a new line before appending the patch
      * @throws Exception
-     *             if errors occured while merging
+     *             if errors occur while merging
      * @author mbrunnli (03.06.2014)
      */
     @Test
@@ -47,7 +62,7 @@ public class TextAppenderTest {
     /**
      * Tests a merge with adding a new line before appending the patch
      * @throws Exception
-     *             if errors occured while merging
+     *             if errors occur while merging
      * @author mbrunnli (03.06.2014)
      */
     @Test
@@ -55,5 +70,106 @@ public class TextAppenderTest {
         TextAppender appender = new TextAppender("", true);
         String mergedString = appender.merge(new File(testFileRootPath + "BaseFile.txt"), "", "UTF-8");
         Assert.assertEquals(FileUtils.readFileToString(new File(testFileRootPath + "BaseFile.txt")), mergedString);
+    }
+
+    /**
+     * Tests if header and footer are excluded in a merge and in the correct position
+     * @throws Exception
+     *             if errors occur while merging
+     */
+    @Test
+    public void testAnchorMerge_excludeHeaderFooter() throws Exception {
+        TextAppender appender = new TextAppender("", false);
+        String mergedString = appender.merge(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt"),
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/PatchDifferentHeaderFooter.txt")),
+            "UTF-8");
+        Assert.assertEquals(
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/MergedCorrectHeaderFooter.txt")),
+            mergedString);
+    }
+
+    /**
+     * Tests if using the mergestrategy "newlinex/xnewline" puts the newline in the correct position
+     * @throws Exception
+     *             if errors occur while merging
+     */
+    @Test
+    public void testAnchorMerge_newLineCorrectOrder() throws Exception {
+        TextAppender appender = new TextAppender("", false);
+        String mergedString = appender.merge(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt"),
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt")), "UTF-8");
+        Assert.assertEquals(
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/MergedCorrectHeaderFooter.txt")),
+            mergedString);
+    }
+
+    /**
+     * Tests if the document parts are in the correct order after merging, e.g. to avoid table ends to come
+     * before table starts
+     * @throws Exception
+     *             if errors occur while merging
+     */
+    @Test
+    public void testAnchorMerge_CorrectOrder() throws Exception {
+        TextAppender appender = new TextAppender("", false);
+        String mergedString = appender.merge(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt"),
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/PatchTestOrder.txt")), "UTF-8");
+        Assert.assertEquals(
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/MergedCorrectOrder.txt")),
+            mergedString);
+    }
+
+    /**
+     * Tests if using the mergestrategy "replace" properly replaces the document part
+     * @throws Exception
+     *             if errors occur while merging
+     */
+    @Test
+    public void testAnchorMerge_replacement() throws Exception {
+        TextAppender appender = new TextAppender("", false);
+        String mergedString = appender.merge(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt"),
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/PatchTestReplacement.txt")), "UTF-8");
+        Assert.assertEquals(FileUtils.readFileToString(new File(testFileRootPath + "anchortests/MergedReplaced.txt")),
+            mergedString);
+    }
+
+    /**
+     * Tests if using the mergestrategy "appendx/xappend" appends the patch in the correct position
+     * @throws Exception
+     *             if errors occur while merging
+     */
+    @Test
+    public void testAnchorMerge_testAppendOrder() throws Exception {
+        TextAppender appender = new TextAppender("", false);
+        String mergedString = appender.merge(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt"),
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/PatchAppend.txt")), "UTF-8");
+        Assert.assertEquals(
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/MergedAppendOrder.txt")), mergedString);
+    }
+
+    /**
+     * Tests if not defining a mergestrategy(anchor:${documentpart}:anchorend) throws an exception
+     * @throws Exception
+     *             if errors occur while merging
+     */
+    @Test
+    public void testAnchorMerge_testEmptyMergestrategyOnlyAppends() throws Exception {
+        TextAppender appender = new TextAppender("", false);
+        String mergedString = appender.merge(new File(testFileRootPath + "anchortests/AnchoredBaseFile.txt"),
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/PatchEmptyMergeStrategy.txt")),
+            "UTF-8");
+        Assert.assertEquals(
+            FileUtils.readFileToString(new File(testFileRootPath + "anchortests/MergedEmptyMergeStrategy.txt")),
+            mergedString);
+    }
+
+    /**
+     * Tests if the method to split the patch by anchor tags actually works as it is supposed to
+     */
+    @Test
+    public void testProperMappingOfAnchorsAndText() {
+        String testString = "// anchor:test:test:anchorend \n line1 \n // anchor:test2:test2:anchorend \n line2";
+        Map<String, String> result = MergeUtil.splitByAnchors(testString);
+        Assert.assertEquals(toBe, result);
     }
 }
