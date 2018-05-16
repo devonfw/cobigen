e3e10d9a263353344bc1fe498fd8b7c96a8a13f9
Shah <krati.shah@capgemini.com>
2018-05-16 12:23:18 +0530

    added additional modules/packages needed for script

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,9 +3,12 @@ import os
 import sys
 
 os.chdir(os.path.dirname(sys.executable))
-
+os.chdir("Scripts")
+os.system('pip install gitpython')
+os.system('pip install PyGithub')
 os.system('pip install lxml')
 os.system('pip install git')
 os.system('pip install pathlib')
 os.system('pip install github')
 os.system('pip install uritemplate')
+os.system('pip install requests')
