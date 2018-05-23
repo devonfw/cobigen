b3648558d0f896b3773b366a1458208bf40ad049
Shah <krati.shah@capgemini.com>
2018-05-24 00:16:37 +0530

    #515 Added user interaction for getting repo and org info

--- scripts/git_authentication.py
+++ scripts/git_authentication.py
@@ -5,21 +5,16 @@ from logging import exception
 
 #This script is responsible for the authentication of git user
 def authenticate_git_user(bool_test):
-    if bool_test:
-        init.git_username = 'XXXXXXX'
-        init.git_password = 'XXXXXXX'
+    init.git_username = input("Enter Your Git User Name: ")
+    while ( (init.git_username in "" )):
+        init.git_username = input("Enter Your Git User Name: ")
+    token_or_password = input("How do you want to authenticate yourself? press t[token] or p[password]: ")
+    while (not (token_or_password in ['t','p'] )):   
+        token_or_password = input("How do you want to authenticate yourself? press t[token] or p[password]: ")
+    if token_or_password =='t':
+        init.git_password = getpass.getpass("Enter Your Git Token: ")
     else:
-	    init.git_username = input("Enter Your Git User Name: ")
-	    while ( (init.git_username in "" )):   
-		    init.git_username = input("Enter Your Git User Name: ")
-		
-	    token_or_password = input("How do you want to authenticate yourself? press t[token] or p[password]: ")
-	    while (not (token_or_password in ['t','p'] )):   
-		    token_or_password = input("How do you want to authenticate yourself? press t[token] or p[password]: ")
-	    if token_or_password =='t':
-		    init.git_password = getpass.getpass("Enter Your Git Token: ")
-	    else:
-		    init.git_password = getpass.getpass("Enter Your Git Password: ")
+        init.git_password = getpass.getpass("Enter Your Git Password: ")
     url = 'https://'+init.git_username+':'+init.git_password+"@api.github.com"
     session = requests.Session()
     message=""
