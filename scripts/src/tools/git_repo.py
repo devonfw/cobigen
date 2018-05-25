fefd1f6c7562ac583e098988612ff42f93779c36
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2018-05-25 17:51:07 +0200

    #515 fixed issue of renamed config entry

--- scripts/src/tools/git_repo.py
+++ scripts/src/tools/git_repo.py
@@ -7,28 +7,29 @@ import sys
 from github.MainClass import Github
 import git
 
+
 class GitRepo:
-    
+
     def __init__(self, config: Config):
         self.config = config
         self.authenticate_git_user()
         self.init_git_repo()
-        
+
         try:
             self.repo = git.cmd.Git(".")
         except InvalidGitRepositoryError:
             print_error("Path is not a git repository. Please go to valid git repository!")
             sys.exit()
 
-    #This script is responsible for the authentication of git user
+    # This script is responsible for the authentication of git user
     def authenticate_git_user(self):    
         authenticated = False
         while not authenticated:
             self.config.git_username = prompt_enter_value("your git user name");
-            self.config.git_password = getpass.getpass("Please enter your password or token: ")
-            
+            self.config.git_password_or_token = getpass.getpass("Please enter your password or token: ")
+
             session = requests.Session()
-            response_object=session.get(self.config.github_api_root_url())
+            response_object = session.get(self.config.github_api_root_url())
             if (response_object.status_code in [201,200]):
                 print_info("Authenticated.")
                 authenticated = True
