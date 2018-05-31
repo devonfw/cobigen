395aa1b3e5d16f1b18a64bf1e2f9ec89a1f4f54c
jdiazgon <jaime.diaz-gonzalez-external@capgemini.com>
2018-05-31 16:58:00 +0200

    #603 merging core with eclipseplugin

---
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/generator/MatcherEvaluatorImpl.java
@@ -0,0 +1,56 @@
+package com.devonfw.cobigen.impl.generator;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.devonfw.cobigen.api.annotation.Cached;
+import com.devonfw.cobigen.api.extension.TriggerInterpreter;
+import com.devonfw.cobigen.api.to.MatcherTo;
+import com.devonfw.cobigen.impl.config.entity.Matcher;
+import com.devonfw.cobigen.impl.config.entity.io.AccumulationType;
+import com.devonfw.cobigen.impl.generator.api.MatcherEvaluator;
+
+/**
+ * Implementation of the matching logic based on propositional logic.
+ */
+public class MatcherEvaluatorImpl implements MatcherEvaluator {
+
+    /** Logger instance. */
+    private static final Logger LOG = LoggerFactory.getLogger(MatcherEvaluatorImpl.class);
+
+    @Cached
+    @Override
+    public boolean matches(Object matcherInput, List<Matcher> matcherList, TriggerInterpreter triggerInterpreter) {
+        boolean matcherSetMatches = false;
+        LOG.debug("Check matchers for TriggerInterpreter[type='{}'] ...", triggerInterpreter.getType());
+        MATCHER_LOOP:
+        for (Matcher matcher : matcherList) {
+            MatcherTo matcherTo = new MatcherTo(matcher.getType(), matcher.getValue(), matcherInput);
+            LOG.trace("Check {} ...", matcherTo);
+            if (triggerInterpreter.getMatcher().matches(matcherTo)) {
+                switch (matcher.getAccumulationType()) {
+                case NOT:
+                    LOG.trace("NOT Matcher matches -> trigger match fails.");
+                    matcherSetMatches = false;
+                    break MATCHER_LOOP;
+                case OR:
+                case AND:
+                    LOG.trace("Matcher matches.");
+                    matcherSetMatches = true;
+                    break;
+                default:
+                }
+            } else {
+                if (matcher.getAccumulationType() == AccumulationType.AND) {
+                    LOG.trace("AND Matcher does not match -> trigger match fails.");
+                    matcherSetMatches = false;
+                    break MATCHER_LOOP;
+                }
+            }
+        }
+        LOG.debug("Matcher declarations " + (matcherSetMatches ? "match the input." : "do not match the input."));
+        return matcherSetMatches;
+    }
+}
---
+++ cobigen/cobigen-core-parent/cobigen-core/src/test/java/com/devonfw/cobigen/unittest/config/upgrade/ContextConfigurationUpgraderTest.java
@@ -0,0 +1,82 @@
+package com.devonfw.cobigen.unittest.config.upgrade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.custommonkey.xmlunit.XMLTestCase;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import com.devonfw.cobigen.api.constants.BackupPolicy;
+import com.devonfw.cobigen.api.constants.ConfigurationConstants;
+import com.devonfw.cobigen.impl.config.constant.ContextConfigurationVersion;
+import com.devonfw.cobigen.impl.config.upgrade.ContextConfigurationUpgrader;
+import com.devonfw.cobigen.unittest.config.common.AbstractUnitTest;
+import com.google.common.io.Files;
+
+/**
+ * Test suite for {@link ContextConfigurationUpgrader}
+ */
+public class ContextConfigurationUpgraderTest extends AbstractUnitTest {
+
+    /** Root path to all resources used in this test case */
+    private static String testFileRootPath =
+        "src/test/resources/testdata/unittest/config/upgrade/ContextConfigurationUpgraderTest/";
+
+    /** JUnit Rule to create and automatically cleanup temporarily files/folders */
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    /**
+     * Tests the valid upgrade of a templates configuration from version v1.2 to v2.1.
+     * @throws Exception
+     *             test fails
+     */
+    @Test
+    public void testCorrectUpgrade_v2_0_TO_v2_1() throws Exception {
+
+        // preparation
+        File tmpTargetConfig = tempFolder.newFile(ConfigurationConstants.CONTEXT_CONFIG_FILENAME);
+        File sourceTestdata =
+            new File(testFileRootPath + "valid-v2.0/" + ConfigurationConstants.CONTEXT_CONFIG_FILENAME);
+        Files.copy(sourceTestdata, tmpTargetConfig);
+
+        ContextConfigurationUpgrader sut = new ContextConfigurationUpgrader();
+
+        ContextConfigurationVersion version = sut.resolveLatestCompatibleSchemaVersion(tempFolder.getRoot().toPath());
+        assertThat(version).as("Source Version").isEqualTo(ContextConfigurationVersion.v2_0);
+
+        sut.upgradeConfigurationToLatestVersion(tempFolder.getRoot().toPath(), BackupPolicy.ENFORCE_BACKUP);
+        assertThat(tmpTargetConfig.toPath().resolveSibling("context.bak.xml").toFile()).exists()
+            .hasSameContentAs(sourceTestdata);
+
+        version = sut.resolveLatestCompatibleSchemaVersion(tempFolder.getRoot().toPath());
+        assertThat(version).as("Target version").isEqualTo(ContextConfigurationVersion.v2_1);
+
+        XMLUnit.setIgnoreWhitespace(true);
+        new XMLTestCase() {
+        }.assertXMLEqual(
+            new FileReader(testFileRootPath + "valid-v2.1/" + ConfigurationConstants.CONTEXT_CONFIG_FILENAME),
+            new FileReader(tmpTargetConfig));
+    }
+
+    /**
+     * Tests the valid upgrade of a templates configuration from version v1.2 to v2.1.
+     * @throws Exception
+     *             test fails
+     */
+    @Test
+    public void testCorrectV2_1SchemaDetection() throws Exception {
+
+        // preparation
+        File targetConfig = new File(testFileRootPath + "valid-v2.1");
+
+        ContextConfigurationVersion version =
+            new ContextConfigurationUpgrader().resolveLatestCompatibleSchemaVersion(targetConfig.toPath());
+        assertThat(version).isEqualTo(ContextConfigurationVersion.v2_1);
+    }
+}
