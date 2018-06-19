bb447be05f2521d7de4a64c083b91938d1d9c8fc
jdiazgon <jaime.diaz-gonzalez-external@capgemini.com>
2018-06-19 08:42:14 +0200

    Adding source target differenciation between connectors

--- cobigen-templates/templates-oasp4j/src/main/java/com/devonfw/cobigen/templates/oasp4j/utils/Connector.java
+++ cobigen-templates/templates-oasp4j/src/main/java/com/devonfw/cobigen/templates/oasp4j/utils/Connector.java
@@ -1,42 +1,63 @@
 package com.devonfw.cobigen.templates.oasp4j.utils;
 
 /**
- * Connector is one association between classes in a class diagram. This class is used for storing that data,
- * for later developing templates.
+ * Connector is one association between classes in a class diagram. This class is used for storing that data, for later
+ * developing templates.
  */
 public class Connector {
 
-    private String className;
+  private String className;
 
-    private String multiplicity;
+  private String multiplicity;
 
-    /**
-     * @param source
-     *            The source of the connector
-     * @param target
-     *            The target of the connector
-     * @param multiplicity
-     *            The multiplicity of the target of this connector
-     */
-    public Connector(String source, String multiplicity) {
-        className = source;
-        this.multiplicity = multiplicity;
-    }
+  private String targetOrSource;
 
-    public String getClassName() {
-        return className;
-    }
+  /**
+   * @param source The source of the connector
+   * @param target The target of the connector
+   * @param multiplicity The multiplicity of the target of this connector
+   */
+  public Connector(String source, String multiplicity, String targetOrSource) {
 
-    public void setClassName(String className) {
-        className = className;
-    }
+    this.className = source;
+    this.multiplicity = multiplicity;
+    this.targetOrSource = targetOrSource;
+  }
 
-    public String getMultiplicity() {
-        return multiplicity;
-    }
+  public String getClassName() {
 
-    public void setMultiplicity(String multiplicity) {
-        this.multiplicity = multiplicity;
-    }
+    return this.className;
+  }
+
+  public void setClassName(String className) {
+
+    className = className;
+  }
+
+  public String getMultiplicity() {
+
+    return this.multiplicity;
+  }
+
+  public void setMultiplicity(String multiplicity) {
+
+    this.multiplicity = multiplicity;
+  }
+
+  /**
+   * @return targetOrSource
+   */
+  public String getTargetOrSource() {
+
+    return this.targetOrSource;
+  }
+
+  /**
+   * @param targetOrSource new value of {@link #gettargetOrSource}.
+   */
+  public void setTargetOrSource(String targetOrSource) {
+
+    this.targetOrSource = targetOrSource;
+  }
 
 }
