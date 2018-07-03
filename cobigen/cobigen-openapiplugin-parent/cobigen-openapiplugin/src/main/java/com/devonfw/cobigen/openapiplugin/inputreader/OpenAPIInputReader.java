acefc2610833bb9f1dcf1c564cb64023aefef526
MikeSchumacherCapgemini <mike.schumacher@capgemini.com>
2018-07-03 10:05:25 +0200

    #689 Renamed field for better clarity

--- cobigen/cobigen-openapiplugin-parent/cobigen-openapiplugin/src/main/java/com/devonfw/cobigen/openapiplugin/inputreader/OpenAPIInputReader.java
+++ cobigen/cobigen-openapiplugin-parent/cobigen-openapiplugin/src/main/java/com/devonfw/cobigen/openapiplugin/inputreader/OpenAPIInputReader.java
@@ -381,7 +381,7 @@ public class OpenAPIInputReader implements InputReader {
         ParameterDef parameter;
         for (Parameter param : parameters) {
             parameter = new ParameterDef();
-            parameter.setInBody(false);
+            parameter.setIsBody(false);
             switch (param.getIn()) {
             case "path":
                 parameter.setInPath(true);
@@ -429,7 +429,7 @@ public class OpenAPIInputReader implements InputReader {
             Schema mediaSchema;
             for (String media : requestBody.getContentMediaTypes().keySet()) {
                 parameter = new ParameterDef();
-                parameter.setInBody(true);
+                parameter.setIsBody(true);
                 parameter.setMediaType(media);
                 if (tags.contains(Constants.SEARCH_CRITERIA)
                     || tags.contains(Constants.SEARCH_CRITERIA.toLowerCase())) {
