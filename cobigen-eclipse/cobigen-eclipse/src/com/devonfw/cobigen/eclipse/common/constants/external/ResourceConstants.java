40126d8e66ae104b3ba0b3df93505a6fcfd13a75
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2018-11-15 18:27:53 +0100

    DEVC-33 #788 adding feedback and fixing other issues

--- cobigen-eclipse/cobigen-eclipse/src/com/devonfw/cobigen/eclipse/common/constants/external/ResourceConstants.java
+++ cobigen-eclipse/cobigen-eclipse/src/com/devonfw/cobigen/eclipse/common/constants/external/ResourceConstants.java
@@ -5,14 +5,20 @@ package com.devonfw.cobigen.eclipse.common.constants.external;
  */
 public class ResourceConstants {
 
-	/**
-	 * Generator Configuration Project Name
-	 */
-	public static final String CONFIG_PROJECT_NAME = "CobiGen_Templates";
+    /**
+     * Generator Configuration Project Name
+     */
+    public static final String CONFIG_PROJECT_NAME = "CobiGen_Templates";
 
-	/**
-	 * Latest Jar folder downloaded from Update Templates
-	 */
-	public static final String DOWNLOADED_JAR_FOLDER = "/.metadata/cobigen_jars";
+    /**
+     * Latest Jar folder downloaded from Update Templates
+     */
+    public static final String DOWNLOADED_JAR_FOLDER = "/.metadata/cobigen_jars";
+
+    /**
+     * Jar name to be used in a file name filter, so that we can check whether the templates are already
+     * downloaded
+     */
+    public static final String JAR_FILE_NAME = "templates-";
 
 }
