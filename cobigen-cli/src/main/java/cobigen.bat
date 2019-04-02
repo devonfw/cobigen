c2c8aacee3092187e35b1ee61465b3d60fde00d7
sanjay <sanjay.h.yadav@capgemini.com>
2019-04-02 18:02:24 +0530

    Need run as admin and provide reference path

--- cobigen-cli/src/main/java/cobigen.bat
+++ cobigen-cli/src/main/java/cobigen.bat
@@ -34,7 +34,7 @@ REM echo setting PATH
 REM setx PATH %JAVA_HOME%\bin;%PATH% -m
 REM echo Display java version
 REM java -version
-set CLASSPATH=.;C:\Program Files\Java\jdk1.8.0_181;C:\Program Files\Java\jdk1.8.0_181\lib\dt.jar;C:\MyData\IDE4\workspaces\com.cobigen.picocli\picocli-2.2.1.jar;C:\Users\syadav9\.m2\repository\com\devonfw\cobigen\openapiplugin\2.2.0\openapiplugin-2.2.0.jar
+set CLASSPATH=.;%JAVA_HOME%;C:\Program Files\Java\jdk1.8.0_181\lib\dt.jar;C:\MyData\IDE4\workspaces\com.cobigen.picocli\picocli-2.2.1.jar;C:\Users\syadav9\.m2\repository\com\devonfw\cobigen\openapiplugin\2.2.0\openapiplugin-2.2.0.jar
 ECHO "Welcome to cobigen and below are increment available for your input" 
 
     
