2d216229608115a293f35f2cac2589b2ad6a6dc2
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2019-04-09 13:09:44 +0200

    Extracting server to local folder

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/externalprocess/ExternalProcessHandler.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/externalprocess/ExternalProcessHandler.java
@@ -1,22 +1,25 @@
 package com.devonfw.cobigen.impl.externalprocess;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.xml.bind.UnmarshalException;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.devonfw.cobigen.api.constants.ExternalProcessConstants;
+import com.devonfw.cobigen.api.exception.CobiGenRuntimeException;
 import com.devonfw.cobigen.impl.exceptions.ConnectionExceptionHandler;
 import com.devonfw.cobigen.impl.exceptions.ConnectionExceptionHandler.ConnectionException;
 import com.devonfw.cobigen.impl.exceptions.HttpConnectionException;
@@ -117,14 +120,38 @@ public class ExternalProcessHandler {
      *            of the executable file to execute
      * @return true if it was able to execute the exe successfully
      */
-    public boolean executingExe(String path) {
+    public boolean executingExe(String path, Class<?> activatorClass) {
 
         exePath = path;
 
         boolean execution = false;
         try {
             LOG.info("Loading server: " + exePath);
-            process = new ProcessBuilder(exePath, String.valueOf(port)).start();
+
+            String filePath = getExePath(activatorClass);
+
+            if (new File(filePath).isFile()) {
+                process = new ProcessBuilder(filePath, String.valueOf(port)).start();
+            } else {
+                filePath = activatorClass.getProtectionDomain().getCodeSource().getLocation().getPath();
+                File jarFile = new File(filePath);
+                String parentDirectory = jarFile.getParent();
+
+                // Is the exe file already available on our directory?
+                if (new File(parentDirectory + exePath).isFile()) {
+                    process = new ProcessBuilder(parentDirectory + exePath, String.valueOf(port)).start();
+                } else {
+                    // We need to extract the file to our current directory
+                    // Do we have write access?
+                    if (Files.isWritable(Paths.get(jarFile.getParent()))) {
+                        Files.copy(getClass().getResourceAsStream(exePath), Paths.get(parentDirectory + exePath));
+                        process = new ProcessBuilder(parentDirectory + exePath, String.valueOf(port)).start();
+                    } else {
+                        // We are not able to extract the server
+                        return false;
+                    }
+                }
+            }
 
             // We try to get the error output
             errorHandler = new ProcessOutputUtil(process.getErrorStream(), "UTF-8");
@@ -147,9 +174,34 @@ public class ExternalProcessHandler {
             LOG.info(parseCause.toString(), e);
             LOG.info("Error starting the external process server", e);
             execution = false;
+            throw new CobiGenRuntimeException("Error starting the external process: " + parseCause.toString());
         }
         return execution;
 
+    }
+
+    /**
+     * @param activatorClass
+     *
+     */
+    private String getExePath(Class<?> activatorClass) {
+        String filePath = "";
+        // We need to load the class that loaded this class because we are getting called from a plug-in
+        // for instance, ts-plugin is calling this class, and the exe is on the classpath of ts-plugin
+        if (getClass().getResource(exePath) == null) {
+
+            if (getClass().getClassLoader().getResource(exePath) == null) {
+                filePath = activatorClass.getProtectionDomain().getCodeSource().getLocation().getPath();
+            } else {
+                filePath = getClass().getClassLoader().getResource(exePath).getPath();
+                return filePath;
+            }
+        } else {
+            filePath = getClass().getResource(exePath).getPath();
+            return filePath;
+        }
+
+        return filePath;
     }
 
     /**
@@ -170,7 +222,6 @@ public class ExternalProcessHandler {
                 }
             } catch (Exception e) {
                 LOG.error("Connection to server failed, attempt number " + retry + ".");
-
                 if (connectionExc.handle(e).equals(ConnectionException.MALFORMED_URL)) {
                     LOG.error("MalformedURLException: Connection to server failed, MalformedURL.", e);
                     return false;
@@ -191,7 +242,7 @@ public class ExternalProcessHandler {
      * @throws IOException
      *             throws {@link IOException}
      */
-    private void startConnection() throws IOException {
+    public void startConnection() throws IOException {
 
         url = new URL("http://" + hostName + ":" + port + "/processmanagement/");
         conn = (HttpURLConnection) url.openConnection();
@@ -204,13 +255,11 @@ public class ExternalProcessHandler {
      *
      * @return true if a port has been acquired
      */
-    public boolean acquirePort() {
+    private boolean acquirePort() {
 
         // If there is any error, probably it is because the port is blocked
         if (isNotConnected() || processHasErrors()) {
-            terminateProcessConnection();
-            port++;
-            executingExe(exePath);
+            restartServer();
         } else {
             return true;
         }
@@ -222,7 +271,7 @@ public class ExternalProcessHandler {
      *
      * @return true if external process contains errors
      */
-    public boolean processHasErrors() {
+    private boolean processHasErrors() {
 
         if (errorHandler != null) {
             // External process has not printed any error
@@ -297,42 +346,82 @@ public class ExternalProcessHandler {
      *            Data to transfer, it should be a serializable class
      * @param conn
      *            {@link HttpURLConnection} to the server, containing also the URL
-     * @param os
-     *            {@link OutputStream} that will be opened for sending data
-     * @param osw
-     *            {@link OutputStreamWriter} used for writing the data to be sent
-     * @return true if the status code was either 200 or 201, false otherwise
-     * @throws IOException
-     *             when connection to the server failed
-     * @throws JsonGenerationException
-     *             When generating the JSON from the transfer object failed
-     * @throws JsonMappingException
-     *             When mapping the JSON from the transfer object failed
+     * @param charsetName
+     *            the name of a supported {@link Charset}
+     * @return returns true when the status code of the request was 200 or 201
      */
-    public boolean sendRequest(Object dataTo, HttpURLConnection conn, OutputStream os, OutputStreamWriter osw)
-        throws IOException, JsonGenerationException, JsonMappingException {
+    public boolean sendRequest(Object dataTo, HttpURLConnection conn, String charsetName) {
         ObjectWriter objWriter;
         objWriter = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String jsonMergerTo = objWriter.writeValueAsString(dataTo);
+        String jsonMergerTo;
+        try (OutputStream os = conn.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os, charsetName);) {
 
-        // We need to escape new lines because otherwise our JSON gets corrupted
-        jsonMergerTo = jsonMergerTo.replace("\\n", "\\\\n");
+            jsonMergerTo = objWriter.writeValueAsString(dataTo);
+            // We need to escape new lines because otherwise our JSON gets corrupted
+            jsonMergerTo = jsonMergerTo.replace("\\n", "\\\\n");
 
-        osw.write(jsonMergerTo);
-        osw.flush();
-        os.close();
-        try {
-            conn.connect();
+            osw.write(jsonMergerTo);
+            osw.flush();
 
-            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK
-                || conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-                throw new HttpConnectionException("Failed : HTTP error code : " + conn.getResponseCode());
+            for (int i = 0; i < ExternalProcessConstants.NUMBER_OF_RETRIES; i++) {
+                conn.connect();
+                int statusCode = conn.getResponseCode();
+
+                if (statusCode == HttpURLConnection.HTTP_OK || statusCode == HttpURLConnection.HTTP_CREATED) {
+                    // Successful request
+                    return true;
+                }
+                if (shouldNotRetry(conn.getResponseCode())) {
+                    return false;
+                }
             }
-        } catch (HttpConnectionException e) {
             return false;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
-        return true;
+        return false;
+
+    }
+
+    /**
+     * Checks whether the HTTP request should NOT be retried
+     * @param statusCode
+     *            code of the HTTP request
+     * @return true when we should not retry because the status code describes a unavoidable case
+     * @throws IOException
+     *             throws {@link IOException}
+     */
+    private boolean shouldNotRetry(int statusCode) throws IOException {
+
+        switch (statusCode) {
+
+        case HttpURLConnection.HTTP_MOVED_TEMP:
+            return false;
+
+        case HttpURLConnection.HTTP_UNAVAILABLE:
+            return false;
+
+        default:
+            try {
+                throw new HttpConnectionException("Failed : HTTP error code : " + conn.getResponseCode());
+            } catch (HttpConnectionException e) {
+                return true;
+            }
+        }
+
+    }
+
+    /**
+     * Terminates the current connection to the server, the process and the error handler. Then tries to load
+     * a new server on a different port number
+     */
+    public void restartServer() {
+        terminateProcessConnection();
+        port++;
+        executingExe(exePath, this.getClass());
     }
 
     /**
@@ -348,9 +437,12 @@ public class ExternalProcessHandler {
         if (process == null) {
             return true;
         }
+        // if (!terminateProcess(process, "nestserver.exe")) {
+        // return false;
+        // }
         if (process.isAlive()) {
             process.destroyForcibly();
-            LOG.info("Closing process");
+            LOG.info("Closing process instance");
         }
         // Let's kill the process error handler
         if (errorHandler.isAlive()) {
