9420fa7b7169b8974e401b60182a753a56f480d5
sanjay <sanjay.h.yadav@capgemini.com>
2019-06-11 15:14:27 +0530

    checked-in for update custom logback

--- cobigen-cli/src/main/java/com/cobigen/picocli/CobiGenCLI.java
+++ cobigen-cli/src/main/java/com/cobigen/picocli/CobiGenCLI.java
@@ -1,11 +1,16 @@
 package com.cobigen.picocli;
 
-import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 
 import com.cobigen.picocli.commands.CobiGenCommand;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
 import picocli.CommandLine;
+import ch.qos.logback.classic.Logger;
 
 /**
  * Starting point of the CobiGen CLI. Contains the main method.
@@ -15,7 +20,7 @@ public class CobiGenCLI {
     /**
      * Logger to output useful information to the user
      */
-    private static Logger logger = LoggerFactory.getLogger(CobiGenCLI.class);
+    private static Logger logger = (Logger) LoggerFactory.getLogger(CobiGenCLI.class);
 
     /**
      * Picocli command line object
@@ -36,7 +41,28 @@ public class CobiGenCLI {
      *            list of arguments the user has passed
      */
     public static void main(String... args) {
+    	/**
+    	 * Customize logger  
+    	 * */
+    	Logger rootLogger = (Logger)LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        LoggerContext loggerContext = rootLogger.getLoggerContext();
+        // we are not interested in auto-configuration
+        { loggerContext.reset() ;}
 
+        PatternLayoutEncoder encoder = new PatternLayoutEncoder();
+        {encoder.setContext(loggerContext);}
+        { encoder.setPattern("%message%n");}
+        {encoder.start();}
+
+        ConsoleAppender<ILoggingEvent> appender = new ConsoleAppender<ILoggingEvent>();
+        { appender.setContext(loggerContext);}
+        {appender.setEncoder(encoder); }
+        {appender.start();}
+
+        {rootLogger.addAppender(appender);}
+        /**
+         * Customization of  logger end 
+         * */
         if (commandLine.execute(args) == 0) {
             logger.debug("Commands were executed correctly");
         }
