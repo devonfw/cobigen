470449cfc953bf6f068642f860b3a5be789597db
Mohamed Ghanmi <mohamed.ghanmi@capgemini.com>
2019-08-14 10:26:57 +0200

    #914 Adding a typescript trigger interpreter

---
+++ cobigen/cobigen-tsplugin/src/main/java/com/devonfw/cobigen/tsplugin/TypeScriptTriggerInterpreter.java
@@ -0,0 +1,42 @@
+package com.devonfw.cobigen.tsplugin;
+
+import com.devonfw.cobigen.api.extension.InputReader;
+import com.devonfw.cobigen.api.extension.MatcherInterpreter;
+import com.devonfw.cobigen.api.extension.TriggerInterpreter;
+import com.devonfw.cobigen.tsplugin.inputreader.TypeScriptInputReader;
+
+/**
+ * {@link TriggerInterpreter} implementation of a Swagger Interpreter
+ */
+public class TypeScriptTriggerInterpreter implements TriggerInterpreter{
+    
+    /**
+     * {@link TriggerInterpreter} type to be registered
+     */
+    public String type;
+
+    /**
+     * Creates a new Swagger Interpreter
+     * @param type
+     *            to be registered
+     */
+    public TypeScriptTriggerInterpreter(String type) {
+        this.type = type;
+    }
+    
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public InputReader getInputReader() {
+        return new TypeScriptInputReader();
+    }
+
+    @Override
+    public MatcherInterpreter getMatcher() {
+        return null;
+    }
+
+}
