13bf4cb1e0a2cccc88bdb4ccbb6e65b9bb829830
sanjay <sanjay.h.yadav@capgemini.com>
2019-08-30 17:12:02 +0530

    remove hardcoded plugin version

--- cobigen-cli/src/main/java/com/devonfw/cobigen/cli/commands/CobiGenCommand.java
+++ cobigen-cli/src/main/java/com/devonfw/cobigen/cli/commands/CobiGenCommand.java
@@ -8,7 +8,7 @@ import picocli.CommandLine.Command;
  * This class defines the main CobiGen command
  */
 @Command(description = MessagesConstants.WELCOME_MESSAGE, name = "cobigen", aliases = { "cg" },
-    mixinStandardHelpOptions = true, version = {"CobiGen CLI 1.0","Java plug-in 2.1.0","OpenAPI plug-in	2.3.0","HTML plug-in 2.0.1"}, subcommands = { GenerateCommand.class })
+    mixinStandardHelpOptions = true, version = "CobiGen CLI 1.0", subcommands = { GenerateCommand.class })
 public class CobiGenCommand implements Runnable {
 
     @Override
