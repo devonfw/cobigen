3b59f7388c066155d3f995506aacac1721f0a21b
sanjay <sanjay.h.yadav@capgemini.com>
2019-08-30 13:27:07 +0530

    changes of review comment

--- cobigen-cli/src/main/java/com/devonfw/cobigen/cli/utils/ParsingUtils.java
+++ cobigen-cli/src/main/java/com/devonfw/cobigen/cli/utils/ParsingUtils.java
@@ -39,182 +39,180 @@ import com.devonfw.cobigen.cli.CobiGenCLI;
  */
 public class ParsingUtils {
 
-    /**
-     * Logger to output useful information to the user
-     */
-    private static Logger logger = LoggerFactory.getLogger(CobiGenCLI.class);
+	/**
+	 * Logger to output useful information to the user
+	 */
+	private static Logger logger = LoggerFactory.getLogger(CobiGenCLI.class);
 
-    /**
-     * Tries to get the Java context by creating a new class loader of the input project that is able to load
-     * the input file. We need this in order to perform reflection on the templates.
-     *
-     * @param inputFile
-     *            input file the user wants to generate code from
-     * @param inputProject
-     *            input project where the input file is located. We need this in order to build the classpath
-     *            of the input file
-     * @return the Java context created from the input project
-     */
-    public static JavaContext getJavaContext(File inputFile, File inputProject) {
+	/**
+	 * Tries to get the Java context by creating a new class loader of the input
+	 * project that is able to load the input file. We need this in order to perform
+	 * reflection on the templates.
+	 *
+	 * @param inputFile    input file the user wants to generate code from
+	 * @param inputProject input project where the input file is located. We need
+	 *                     this in order to build the classpath of the input file
+	 * @return the Java context created from the input project
+	 */
+	public static JavaContext getJavaContext(File inputFile, File inputProject) {
 
-        JavaContext context = JavaSourceProviderUsingMaven.createFromLocalMavenProject(inputProject,true);
-        String fqn = ParsingUtils.getFQN(inputFile);
+		JavaContext context = JavaSourceProviderUsingMaven.createFromLocalMavenProject(inputProject, true);
+		String fqn = ParsingUtils.getFQN(inputFile);
 		try {
 			context.getClassLoader().loadClass(fqn);
 		} catch (NoClassDefFoundError | ClassNotFoundException e) {
-            logger.error("Compiled class " + e.getMessage()
-                + " has not been found. Most probably you need to build project " + inputProject.toString() + " .");
-            System.exit(1);
-        }
-        return context;
-    }
-    /**
-     * This method is traversing parent folders until it reaches java folder in order to get the FQN
-     * @param inputFile
-     * @return qualified name with full package
-     */
-    private static String getFQN(File inputFile) {
-        String simpleName = inputFile.getName().replaceAll("\\.(?i)java", "");
-        String packageName = getPackageName(inputFile.getParentFile(), "");
+			logger.error("Compiled class " + e.getMessage()
+					+ " has not been found. Most probably you need to build project " + inputProject.toString() + " .");
+			System.exit(1);
+		}
+		return context;
+	}
 
-         return packageName + "." + simpleName;
-    }
-/**
- *  This method traverse the folder in reverse order from child to parent
- *  @param folder parent input file
- *  @param packageName 
- *  @return package name
- * */
-     
+	/**
+	 * This method is traversing parent folders until it reaches java folder in
+	 * order to get the FQN
+	 * 
+	 * @param inputFile
+	 * @return qualified name with full package
+	 */
+	private static String getFQN(File inputFile) {
+		String simpleName = inputFile.getName().replaceAll("\\.(?i)java", "");
+		String packageName = getPackageName(inputFile.getParentFile(), "");
+
+		return packageName + "." + simpleName;
+	}
+
+	/**
+	 * This method traverse the folder in reverse order from child to parent
+	 * 
+	 * @param folder      parent input file
+	 * @param packageName
+	 * @return package name
+	 */
 	private static String getPackageName(File folder, String packageName) {
 
-         if (folder == null) {
-            return null;
-        }
+		if (folder == null) {
+			return null;
+		}
 
-         if (folder.getName().toLowerCase().equals("java")) {
-            String[] pkgs = packageName.split("\\.");
+		if (folder.getName().toLowerCase().equals("java")) {
+			String[] pkgs = packageName.split("\\.");
 
-             packageName = pkgs[pkgs.length - 1];
-            // Reverse order as we have traversed folders from child to parent
-            for (int i = pkgs.length - 2; i > 0; i--) {
-                packageName = packageName + "." + pkgs[i];
-            }
-            return packageName;
-        }
-        return getPackageName(folder.getParentFile(), packageName + "." + folder.getName());
-    }
+			packageName = pkgs[pkgs.length - 1];
+			// Reverse order as we have traversed folders from child to parent
+			for (int i = pkgs.length - 2; i > 0; i--) {
+				packageName = packageName + "." + pkgs[i];
+			}
+			return packageName;
+		}
+		return getPackageName(folder.getParentFile(), packageName + "." + folder.getName());
+	}
 
-    /**
-     * Creates a new {@link BasePackage}
-     * @param source
-     *            of the current context {@link BaseSource}
-     * @param qName
-     *            the qualified name of the parent {@link CodeName}
-     * @return A new {@link BasePackage}
-     */
-    private static BasePackage createPackage(BaseSource source, CodeName qName) {
+	/**
+	 * Creates a new {@link BasePackage}
+	 * 
+	 * @param source of the current context {@link BaseSource}
+	 * @param qName  the qualified name of the parent {@link CodeName}
+	 * @return A new {@link BasePackage}
+	 */
+	private static BasePackage createPackage(BaseSource source, CodeName qName) {
 
-        BasePackage parentPkg = source.getRootPackage();
-        if (qName == null) {
-            return parentPkg;
-        }
-        String simpleName = qName.getSimpleName();
-        CodeName parentName = qName.getParent();
-        if (parentName == null) {
-            if (simpleName.isEmpty()) {
-                return parentPkg;
-            }
-        } else {
-            parentPkg = createPackage(source, parentName);
-        }
-        return new BasePackage(parentPkg, simpleName, null, null, false);
-    }
+		BasePackage parentPkg = source.getRootPackage();
+		if (qName == null) {
+			return parentPkg;
+		}
+		String simpleName = qName.getSimpleName();
+		CodeName parentName = qName.getParent();
+		if (parentName == null) {
+			if (simpleName.isEmpty()) {
+				return parentPkg;
+			}
+		} else {
+			parentPkg = createPackage(source, parentName);
+		}
+		return new BasePackage(parentPkg, simpleName, null, null, false);
+	}
 
-    /**
-     * Creates a new {@link BaseFile}
-     * @param className
-     *            name of the class
-     * @param context
-     *            current java project context
-     * @return A new {@link BaseFile}
-     */
-    private static BaseFile createFile(String className, JavaContext context) {
+	/**
+	 * Creates a new {@link BaseFile}
+	 * 
+	 * @param className name of the class
+	 * @param context   current java project context
+	 * @return A new {@link BaseFile}
+	 */
+	private static BaseFile createFile(String className, JavaContext context) {
 
-        CodeName qName = context.parseName(className);
-        BasePackage pkg = createPackage(context.getSource(), qName.getParent());
-        return new BaseFile(pkg, qName.getSimpleName());
-    }
+		CodeName qName = context.parseName(className);
+		BasePackage pkg = createPackage(context.getSource(), qName.getParent());
+		return new BaseFile(pkg, qName.getSimpleName());
+	}
 
-    /**
-     * Gets the full qualified name of the input file
-     * @param inputFile
-     *            Java file we want to get its qualified name
-     * @param context
-     *            current java project context
-     * @return The full qualified name of the input file
-     */
-    public static String getQualifiedName(File inputFile, JavaContext context) {
+	/**
+	 * Gets the full qualified name of the input file
+	 * 
+	 * @param inputFile Java file we want to get its qualified name
+	 * @param context   current java project context
+	 * @return The full qualified name of the input file
+	 */
+	public static String getQualifiedName(File inputFile, JavaContext context) {
 
-        String className = inputFile.getName().replace(".java", "");
-        BaseFile file = createFile(className, context);
-        JavaSourceCodeParserImpl parser = new JavaSourceCodeParserImpl(); 
-       
-        try (Reader reader = new FileReader(inputFile.getAbsolutePath())) {        	
-            BaseType parseType = parser.parseType(reader, file);
+		String className = inputFile.getName().replace(".java", "");
+		BaseFile file = createFile(className, context);
+		JavaSourceCodeParserImpl parser = new JavaSourceCodeParserImpl();
+
+		try (Reader reader = new FileReader(inputFile.getAbsolutePath())) {
+			BaseType parseType = parser.parseType(reader, file);
 			return parseType.getQualifiedName();
-            
-        	
-        } catch (IOException e) {
-            throw new RuntimeIoException(e, IoMode.READ);
-        }
-    }
 
-    /**
-     * Tries to parse a relative path with the current working directory
-     * @param inputFiles
-     *            list of all input files from the user
-     *
-     * @param inputFile
-     *            input file which we are going to parse to find out whether it is a valid file
-     * @param index
-     *            location of the input file in the ArrayList of inputs
-     * @return true only if the parsed file exists, false otherwise
-     *
-     */
-    public static Boolean parseRelativePath(ArrayList<File> inputFiles, File inputFile, int index) {
-        try {
-            Path inputFilePath = Paths.get(System.getProperty("user.dir"), inputFile.toString());
+		} catch (IOException e) {
+			throw new RuntimeIoException(e, IoMode.READ);
+		}
+	}
 
-            if (inputFilePath.toFile().exists()) {
-                inputFiles.set(index, inputFilePath.toFile());
-                return true;
-            }
-        } catch (InvalidPathException e) {
-            logger.debug("The path string " + System.getProperty("user.dir") + " + " + inputFile.toString()
-                + " cannot be converted to a path");
-        }
-        return false;
-    }
+	/**
+	 * Tries to parse a relative path with the current working directory
+	 * 
+	 * @param inputFiles list of all input files from the user
+	 *
+	 * @param inputFile  input file which we are going to parse to find out whether
+	 *                   it is a valid file
+	 * @param index      location of the input file in the ArrayList of inputs
+	 * @return true only if the parsed file exists, false otherwise
+	 *
+	 */
+	public static Boolean parseRelativePath(ArrayList<File> inputFiles, File inputFile, int index) {
+		try {
+			Path inputFilePath = Paths.get(System.getProperty("user.dir"), inputFile.toString());
 
-    /**
-     * Tries to find the root folder of the project in order to build the classpath. This method is trying to
-     * find the first pom.xml file and then getting the folder where is located
-     *
-     * @param inputFile
-     *            passed by the user
-     * @return the project folder
-     *
-     */
-    public static File getProjectRoot(File inputFile) {
+			if (inputFilePath.toFile().exists()) {
+				inputFiles.set(index, inputFilePath.toFile());
+				return true;
+			}
+		} catch (InvalidPathException e) {
+			logger.debug("The path string " + System.getProperty("user.dir") + " + " + inputFile.toString()
+					+ " cannot be converted to a path");
+		}
+		return false;
+	}
 
-        File pomFile = ValidationUtils.findPom(inputFile);
-        if (pomFile != null) {
-            return pomFile.getParentFile();
-        }
-        logger.debug("Projec root could not be found, therefore we use your current input file location.");
-        logger.debug("Using '" + inputFile.getParent() + "' as location where code will be generated");
-        return inputFile.getAbsoluteFile().getParentFile();
-    }
+	/**
+	 * Tries to find the root folder of the project in order to build the classpath.
+	 * This method is trying to find the first pom.xml file and then getting the
+	 * folder where is located
+	 *
+	 * @param inputFile passed by the user
+	 * @return the project folder
+	 *
+	 */
+	public static File getProjectRoot(File inputFile) {
+
+		File pomFile = ValidationUtils.findPom(inputFile);
+		if (pomFile != null) {
+			return pomFile.getParentFile();
+		}
+		logger.debug("Projec root could not be found, therefore we use your current input file location.");
+		logger.debug("Using '" + inputFile.getParent() + "' as location where code will be generated");
+		return inputFile.getAbsoluteFile().getParentFile();
+	}
 
 }
