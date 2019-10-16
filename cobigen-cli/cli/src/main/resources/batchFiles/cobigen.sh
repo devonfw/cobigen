87c2f36870b9f1a9e59d462765fdadadcbbe06ff
Jaime Diaz <jaime.diaz-gonzalez-external@capgemini.com>
2019-10-16 12:40:07 +0200

    Fixing wrong sh files

--- cobigen-cli/cli/src/main/resources/batchFiles/cobigen.sh
+++ cobigen-cli/cli/src/main/resources/batchFiles/cobigen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$*"=="" ]; then
+if [ -z "$*" ]; then
     java -jar $SOFTWARE_PATH/cobigen-cli/cobigen.jar -help
 else
-    java -javaagent:%SOFTWARE_PATH%\cobigen-cli\class-loader-agent.jar -jar %SOFTWARE_PATH%\cobigen-cli\cobigen.jar $*
+    java -javaagent:$SOFTWARE_PATH/cobigen-cli/class-loader-agent.jar -jar $SOFTWARE_PATH/cobigen-cli/cobigen.jar $*
 fi
