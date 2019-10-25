7bf8b22a635c732f12af0eb3fbd90c88629a2561
Mohamed Ghanmi <mohamed.ghanmi@capgemini.com>
2019-10-25 16:25:12 +0200

    #958 test code documentation

--- cobigen-cli/cli/src/test/java/com/devonfw/cobigen/cli/commandtests/UpdateCommandTest.java
+++ cobigen-cli/cli/src/test/java/com/devonfw/cobigen/cli/commandtests/UpdateCommandTest.java
@@ -35,94 +35,96 @@ import classloader.Agent;
  * 
  */
 public class UpdateCommandTest {
-    
+
     /**
      * Logger to output useful information to the user
      */
     private static Logger logger = LoggerFactory.getLogger(CobiGenCLI.class);
-    
+
     /** Test resources root path */
     private static String testFileRootPath = "src/test/resources/testdata/";
-    
+
     /** pom file name */
     private static String pomFileName = "pom.xml";
-    
+
     /** temporary pom file */
     private static String tmpPomFileName = "tmpPom.xml";
-    
+
     /** outdated pom file */
     private static String outdatedPomFileName = "outdatedPom.xml";
-    
+
     /** root CLI path */
     private static Path rootCLIPath = null;
-    
+
     /**
-     * We need to dynamically load the Java agent before the tests.
-     * Note that Java 9 requires -Djdk.attach.allowAttachSelf=true to be present among JVM startup arguments.
+     * We need to dynamically load the Java agent before the tests. Note that Java 9 requires
+     * -Djdk.attach.allowAttachSelf=true to be present among JVM startup arguments.
      */
     @Before
     public void loadJavaAgent() {
         AgentLoader.loadAgentClass(Agent.class.getName(), "");
     }
-    
-    /** TODO: rootCLI path setting 
-     * 
+
+    /**
+     * Sets of the correct CLI root path.
      * @throws URISyntaxException
      */
     @Before
-    public void setCliPath() throws URISyntaxException {
-        if (rootCLIPath == null)
-        {
+    public static void setCliPath() throws URISyntaxException {
+        if (rootCLIPath == null) {
             File locationCLI;
             locationCLI = new File(GenerateCommand.class.getProtectionDomain().getCodeSource().getLocation().toURI());
             rootCLIPath = locationCLI.getParentFile().toPath();
         }
     }
-    
+
     /**
-     * TODO: Repalce original pom.
-     * @throws URISyntaxException 
-     * @throws IOException 
+     * Replaces the original pom with and outdated one.
+     * @throws URISyntaxException
+     * @throws IOException
      */
     @Before
-    public void replacePom() throws IOException  {
+    public void replacePom() throws IOException {
 
-        File originalPom = new File(Paths.get(rootCLIPath.toString(),pomFileName).toString());
-        logger.info("Pom file path" + rootCLIPath.toString());        
-        File tmpPom = new File(Paths.get(testFileRootPath,tmpPomFileName).toString());
-        
-        // Storing original pom 
+        File originalPom = new File(Paths.get(rootCLIPath.toString(), pomFileName).toString());
+        logger.info("Pom file path" + rootCLIPath.toString());
+        File tmpPom = new File(Paths.get(testFileRootPath, tmpPomFileName).toString());
+
+        // Storing original pom
         FileUtils.copyFile(originalPom, tmpPom);
         // Replacing the original pom with an outdated one
-        File outdatedPom = new File(Paths.get(testFileRootPath,outdatedPomFileName).toString());
-        FileUtils.copyFile(outdatedPom,originalPom);
-        
+        File outdatedPom = new File(Paths.get(testFileRootPath, outdatedPomFileName).toString());
+        FileUtils.copyFile(outdatedPom, originalPom);
+
     }
-    
-    
+
     /**
-     * TODO: Restore original pom.
-     * @throws URISyntaxException 
-     * @throws IOException 
+     * Restores the original pom.
+     * @throws URISyntaxException
+     * @throws IOException
      */
-    
+
     @After
     public void restorePom() throws IOException {
-        File originalPom = new File(Paths.get(rootCLIPath.toString(),pomFileName).toString());
-        File tmpPom = new File(Paths.get(testFileRootPath,tmpPomFileName).toString());
-        
-        // Restoring original pom 
-        FileUtils.copyFile(tmpPom,originalPom);
-        
+        File originalPom = new File(Paths.get(rootCLIPath.toString(), pomFileName).toString());
+        File tmpPom = new File(Paths.get(testFileRootPath, tmpPomFileName).toString());
+
+        // Restoring original pom
+        FileUtils.copyFile(tmpPom, originalPom);
+
     }
-    
+
     /**
-     *  Read pom file
+     * Reads the given pom file and extracts the dependencies.
+     * @param pomFile
+     *            input pom file
+     * @return a list of dependencies
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws XmlPullParserException
      */
-    private List<Dependency> readPom(File pomFile) throws FileNotFoundException, IOException, XmlPullParserException
-    {
-        if (pomFile.exists())
-        {
+    private List<Dependency> readPom(File pomFile) throws FileNotFoundException, IOException, XmlPullParserException {
+        if (pomFile.exists()) {
             MavenXpp3Reader reader = new MavenXpp3Reader();
             Model model = reader.read(new FileReader(pomFile));
             List<Dependency> pomDependencies = model.getDependencies();
@@ -131,60 +133,60 @@ public class UpdateCommandTest {
         return new ArrayList<>();
 
     }
-    
+
     /**
-     * TODO
+     * Extracts the plugin's version from the given pom file.
      * @param artifactId
-     * @return
+     *            plugin id
+     * @return the plugin version
      */
     private String getArtifactVersion(File pomFile, String artifactId) {
         String version = null;
-        
+
         List<Dependency> dependencies;
         try {
             dependencies = readPom(pomFile);
-            
             // Get plugin version
-            Optional<Dependency> matchingObject = dependencies.stream().
-                filter(p -> p.getArtifactId().equals(artifactId)).
-                findFirst();
-            
+            Optional<Dependency> matchingObject =
+                dependencies.stream().filter(p -> p.getArtifactId().equals(artifactId)).findFirst();
+
             version = matchingObject.get().getVersion();
         } catch (IOException | XmlPullParserException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return version;
     }
 
     /**
-     * Update test
-     * @throws URISyntaxException 
-     * @throws IOException 
-     * @throws XmlPullParserException 
+     * Plugin update test. The original pom is replaced with an outdated one that needs to updated. The
+     * outdated pom gets updated. The tests checks whether the updating process was successful by comparing
+     * the versions of the updated plugins.
+     * @throws URISyntaxException
+     * @throws IOException
+     * @throws XmlPullParserException
      */
     @Test
     public void pluginUpdateTest() throws IOException, XmlPullParserException {
         String pluginId = "tsplugin";
-        File originalPom = new File(Paths.get(rootCLIPath.toString(),pomFileName).toString());
-       
+        File originalPom = new File(Paths.get(rootCLIPath.toString(), pomFileName).toString());
+
         String oldVersion = getArtifactVersion(originalPom, pluginId);
         logger.info("tsplugin version before update: " + oldVersion);
-        
+
         assertNotNull(oldVersion);
-        
+
         String args[] = new String[1];
         args[0] = "update";
         CobiGenCLI.main(args);
-        
-        File updatedPom = new File(Paths.get(rootCLIPath.toString(),pomFileName).toString());       
+
+        File updatedPom = new File(Paths.get(rootCLIPath.toString(), pomFileName).toString());
         String newVersion = getArtifactVersion(updatedPom, pluginId);
-        
+
         assertNotNull(newVersion);
         assertNotEquals(oldVersion, newVersion);
-        
+
         logger.info("tsplugin version after update: " + newVersion);
-        
+
     }
 
 }
