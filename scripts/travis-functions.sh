ca7ea1ef46406d4d821508bd9c069a3b445e69aa
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2020-02-24 23:15:00 +0100

    #541 fixed functions

--- scripts/travis-functions.sh
+++ scripts/travis-functions.sh
@@ -1,14 +1,27 @@
 function justOneFolderChanged { args : string folderName } {
 	# split will return a list with one element (the empty string) if called on an empty string
-	DIFF_FILES="$(git diff --name-only origin/master | xargs)")
+	DIFF_FILES="$(git diff --name-only origin/master | xargs)"
 	while IFS=$' \t\n' read -ra DIFF_FILES; do
     for i in "${ADDR[@]}"; do
-      # process "$i"
-      if [[ $i == $folderName* ]]; then
+      if [[ $i != $folderName* ]]; then
         echo "'$i' does not start with /$folderName"
         return false
       fi
     done
 	done
 	return true
+}
+
+function folderChanged { args : string folderName } {
+	# split will return a list with one element (the empty string) if called on an empty string
+	DIFF_FILES="$(git diff --name-only origin/master | xargs)"
+	while IFS=$' \t\n' read -ra DIFF_FILES; do
+    for i in "${ADDR[@]}"; do
+      if [[ $i == $folderName* ]]; then
+        echo "'$i' has been changed within /$folderName"
+        return true
+      fi
+    done
+	done
+	return false
 }
