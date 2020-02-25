4bedbe8287cb5b1c4fa4aaf64c4c6d3ac86f96d2
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2020-02-25 09:21:17 +0100

    #541 fix functions

--- scripts/travis-functions.sh
+++ scripts/travis-functions.sh
@@ -1,27 +1,25 @@
 function justOneFolderChanged() {
 	# split will return a list with one element (the empty string) if called on an empty string
 	DIFF_FILES="$(git diff --name-only origin/master | xargs)"
-	while IFS=$' \t\n' read -ra DIFF_FILES; do
-    for i in "${ADDR[@]}"; do
-      if [[ $i != $1* ]]; then
-        echo "'$i' does not start with /$1"
-        return false
-      fi
-    done
-	done
-	return true
+  IFS=$'\n'
+  for i in $DIFF_FILES; do
+    if [[ $i != $1* ]]; then
+      echo "'$i' does not start with /$1"
+      return 1
+    fi
+  done
+	return 0
 }
 
 function folderChanged() {
 	# split will return a list with one element (the empty string) if called on an empty string
 	DIFF_FILES="$(git diff --name-only origin/master | xargs)"
-	while IFS=$' \t\n' read -ra DIFF_FILES; do
-    for i in "${ADDR[@]}"; do
-      if [[ $i == $1* ]]; then
-        echo "'$i' has been changed within /$1"
-        return true
-      fi
-    done
-	done
-	return false
+	IFS=$'\n'
+  for i in $DIFF_FILES; do
+    if [[ $i == $1* ]]; then
+      echo "'$i' has been changed within /$1"
+      return 0
+    fi
+  done
+	return 1
 }
