395f213723b1e7cea7d0c9c5521d6e744970ab00
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2020-03-17 01:11:12 +0100

    #541 fix build path depending on branch

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -5,32 +5,33 @@ source ./scripts/travis-functions.sh
 
 ROOT=""
 
-if [ $TRAVIS_BRANCH == "master" ]; then 
+if [[ $TRAVIS_BRANCH == "master" ]]; then 
   if justOneFolderChanged "cobigen-templates/"; then
     echo "Just Templates changed!"
     ROOT="cobigen-templates"
   else
     ROOT=""
   fi
-elif [ $TRAVIS_BRANCH == "dev_eclipseplugin" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_eclipseplugin" ]]; then
   ROOT="cobigen-eclipse"
-elif [ $TRAVIS_BRANCH == "dev_htmlmerger" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_htmlmerger" ]]; then
   ROOT="cobigen/cobigen-htmlplugin"
-elif [ $TRAVIS_BRANCH == "dev_mavenplugin" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_mavenplugin" ]]; then
   ROOT="cobigen-maven"
-elif [ $TRAVIS_BRANCH == "dev_tempeng_freemarker" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_tempeng_freemarker" ]]; then
   ROOT="cobigen/cobigen-templateengines/cobigen-tempeng-freemarker"
-elif [ $TRAVIS_BRANCH == "dev_tempeng_velocity" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_tempeng_velocity" ]]; then
   ROOT="cobigen/cobigen-templateengines/cobigen-tempeng-velocity"
-elif [ $TRAVIS_BRANCH == "dev_core" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_core" ]]; then
   ROOT="cobigen/cobigen-core-parent"
-elif [ $TRAVIS_BRANCH == "dev_javaplugin" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_javaplugin" ]]; then
   ROOT="cobigen/cobigen-javaplugin-parent"
-elif [ $TRAVIS_BRANCH == "dev_openapiplugin" ]; then
+elif [[ $TRAVIS_BRANCH == "dev_openapiplugin" ]]; then
   ROOT="cobigen/cobigen-openapiplugin-parent"
 else
   ROOT="cobigen/cobigen-${TRAVIS_BRANCH/dev_/}"
 fi
+echo "Execute build in $ROOT"
 
 # -s ${MAVEN_SETTINGS} 
 if [ $TRAVIS_BRANCH == 'master' ]; then
