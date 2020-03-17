237dde84e16f6ebdf88ef09eab45030e4e034298
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2020-03-17 22:37:19 +0100

    #541 add continous deployment into sonatype repositories

--- scripts/travis-functions.sh
+++ scripts/travis-functions.sh
@@ -23,3 +23,41 @@ function folderChanged() {
   done
 	return 1
 }
+
+function navigateToBuildRoot() {
+  ROOT=""
+
+  if [[ $TRAVIS_BRANCH == "master" ]]; then 
+    if justOneFolderChanged "cobigen-templates/"; then
+      echo "Just Templates changed!"
+      ROOT="cobigen-templates"
+    else
+      ROOT=""
+    fi
+  elif [[ $TRAVIS_BRANCH == "dev_eclipseplugin" ]]; then
+    ROOT="cobigen-eclipse"
+  elif [[ $TRAVIS_BRANCH == "dev_htmlmerger" ]]; then
+    ROOT="cobigen/cobigen-htmlplugin"
+  elif [[ $TRAVIS_BRANCH == "dev_mavenplugin" ]]; then
+    ROOT="cobigen-maven"
+  elif [[ $TRAVIS_BRANCH == "dev_tempeng_freemarker" ]]; then
+    ROOT="cobigen/cobigen-templateengines/cobigen-tempeng-freemarker"
+  elif [[ $TRAVIS_BRANCH == "dev_tempeng_velocity" ]]; then
+    ROOT="cobigen/cobigen-templateengines/cobigen-tempeng-velocity"
+  elif [[ $TRAVIS_BRANCH == "dev_core" ]]; then
+    ROOT="cobigen/cobigen-core-parent"
+  elif [[ $TRAVIS_BRANCH == "dev_javaplugin" ]]; then
+    ROOT="cobigen/cobigen-javaplugin-parent"
+  elif [[ $TRAVIS_BRANCH == "dev_openapiplugin" ]]; then
+    ROOT="cobigen/cobigen-openapiplugin-parent"
+  else
+    ROOT="cobigen/cobigen-${TRAVIS_BRANCH/dev_/}"
+  fi
+
+  if [ -z "$ROOT" ]; then
+    echo "Execute build in root directory of the repository"
+  else
+    echo "Execute build in $ROOT"
+    cd $ROOT
+  fi
+}
