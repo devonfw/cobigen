ecd73c092e0ae5f93795db3b031e958aa1bca327
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2020-03-26 18:06:08 +0100

    added devon4j-template unit tests (copied and adjusted tests from oasp4j-templates)

---
+++ cobigen-maven/cobigen-maven-systemtest/src/test/resources/testdata/systemtest/Devon4JTemplateTest/TestPackageInputDataaccessGeneration/src/main/java/io/github/devonfw/cobigen/generator/dataaccess/api/InputEntity.java
@@ -0,0 +1,25 @@
+package io.github.devonfw.cobigen.generator.dataaccess.api;
+
+public class InputEntity {
+    
+    private String input;
+    
+    private int complexity;
+    
+    public String getInput() {
+        return input;
+    }
+    
+    public void setInput(String input) {
+        this.input = input;
+    }
+    
+    public int getComplexity() {
+        return complexity;
+    }
+    
+    public void setComplexity(int complexity) {
+        this.complexity = complexity;
+    }
+    
+}
