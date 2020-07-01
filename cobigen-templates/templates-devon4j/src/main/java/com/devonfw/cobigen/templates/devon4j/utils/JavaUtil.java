d05ea7aebe2f5884a983a676b0d30945d1030c27
Lilli <caothivananh98@gmail.com>
2020-07-01 16:09:27 +0200

    generate sql script from entity

--- cobigen-templates/templates-devon4j/src/main/java/com/devonfw/cobigen/templates/devon4j/utils/JavaUtil.java
+++ cobigen-templates/templates-devon4j/src/main/java/com/devonfw/cobigen/templates/devon4j/utils/JavaUtil.java
@@ -3,10 +3,20 @@ package com.devonfw.cobigen.templates.devon4j.utils;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -489,5 +499,139 @@ public class JavaUtil {
             LOG.warn("{}: Could not find {}", e.getMessage(), className);
             return null;
         }
+    }
+
+    /**
+     * Get the annotated table name of a given Entity class
+     *
+     * @param className
+     *            full qualified class name
+     * @return the annotated table name if existed or class name without the word Entity
+     */
+    public String getEntityTableName(String className) {
+        try {
+            Class<?> entityClass = Class.forName(className);
+            Table table = entityClass.getAnnotation(Table.class);
+            return table == null
+                ? StringUtils.left(entityClass.getSimpleName(),
+                    entityClass.getSimpleName().length() - "Entity".length())
+                : table.name();
+        } catch (ClassNotFoundException e) {
+            LOG.error("{}: Could not find {}", e.getMessage(), className);
+            return null;
+        }
+    }
+
+    /**
+     * Helper method to get all fields recursively inclusive fields from super classes
+     *
+     * @param cl
+     *            class to find fields
+     * @param fields
+     *            list of fields to accumulate recursively
+     * @return list of all fields found
+     */
+    private static List<Field> getAllFields(List<Field> fields, Class<?> cl) {
+        fields.addAll(Arrays.asList(cl.getDeclaredFields()));
+
+        if (cl.getSuperclass() != null) {
+            getAllFields(fields, cl.getSuperclass());
+        }
+
+        return fields;
+    }
+
+    /**
+     * Helper method to get type of a field inclusive field from super classes
+     *
+     * @param pojoClass
+     *            {@link Class} the class object of the pojo
+     * @param fieldName
+     *            {@link String} the name of the field
+     * @return type of the field
+     */
+    private Class<?> getTypeOfField(Class<?> pojoClass, String fieldName) {
+        if (pojoClass != null) {
+            List<Field> fields = new ArrayList();
+            getAllFields(fields, pojoClass);
+
+            Optional<Field> field = fields.stream().filter(f -> f.getName().equals(fieldName)).findFirst();
+
+            if (field.isPresent()) {
+                return field.get().getType();
+            }
+        }
+        LOG.error("Could not find type of field {}", fieldName);
+        return null;
+    }
+
+    /**
+     * @param pojoClass
+     *            {@link Class} the class object of the pojo
+     * @param fieldName
+     *            {@link String} the name of the field
+     * @return true if the field is an instance of java.utils.Collections
+     */
+    public boolean isCollection2(Class<?> pojoClass, String fieldName) {
+        Class<?> type = getTypeOfField(pojoClass, fieldName);
+        return type == null ? false : Collection.class.isAssignableFrom(type);
+    }
+
+    /**
+     * @param className
+     *            {@link String} full qualified class name
+     * @param fieldName
+     *            {@link String} the name of the field
+     * @return type of the field in String
+     */
+    public String getCanonicalNameOfField(String className, String fieldName) {
+        try {
+            Class<?> entityClass = Class.forName(className);
+            Class<?> type = getTypeOfField(entityClass, fieldName);
+            if (type != null) {
+                return type.getCanonicalName();
+            }
+        } catch (ClassNotFoundException e) {
+            LOG.error("{}: Could not find {}", e.getMessage(), className);
+        }
+        return null;
+    }
+
+    /**
+     * Get the primary key and its type of a given Entity class
+     *
+     * @param className
+     *            full qualified class name
+     * @return the primary key and its type if found or null
+     */
+    public String getPrimaryKey(String className) {
+        try {
+            Class<?> entityClass = Class.forName(className);
+            List<Field> fields = new ArrayList();
+            getAllFields(fields, entityClass);
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Id.class)) {
+                    return field.getType().getCanonicalName() + ","
+                        + (field.isAnnotationPresent(Column.class) ? field.getAnnotation(Column.class).name()
+                            : field.getName());
+                } else {
+                    Optional<Method> getterOptional = Arrays.stream(entityClass.getMethods())
+                        .filter(m -> m.getName().equals(
+                            "get" + field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1))
+                            && m.isAnnotationPresent(Id.class))
+                        .findFirst();
+                    if (getterOptional.isPresent()) {
+                        Method getter = getterOptional.get();
+                        return getter.getReturnType().getCanonicalName() + ","
+                            + (getter.isAnnotationPresent(Column.class) ? getter.getAnnotation(Column.class).name()
+                                : field.getName());
+                    }
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            LOG.error("{}: Could not find {}", e.getMessage(), className);
+        }
+        LOG.error("Could not find the field or getter with @Id annotated");
+        return null;
     }
 }
