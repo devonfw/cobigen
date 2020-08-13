e9a26f5b865fc41ccfcadaf611a3118fdd14c5c1
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2020-08-13 18:28:08 +0200

    declared Hashmap as a Map made sure to return a valid external inputreader, while building the readableCache moved redundant code to new getValidInputReader method

--- cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/generator/InputInterpreterImpl.java
+++ cobigen/cobigen-core-parent/cobigen-core/src/main/java/com/devonfw/cobigen/impl/generator/InputInterpreterImpl.java
@@ -5,6 +5,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.inject.Inject;
@@ -69,49 +70,75 @@ public class InputInterpreterImpl implements InputInterpreter {
     public Object read(Path path, Charset inputCharset, Object... additionalArguments) throws InputReaderException {
         Set<String> keySet = PluginRegistry.getTriggerInterpreterKeySet();
         // We first try to find an input reader that is most likely readable
-        HashMap<String, Boolean> readableCache = new HashMap<String, Boolean>();
+        Map<String, Boolean> readableCache = new HashMap<>();
+        Object readable = null;
 
         // Create cache for readable states
         for (String s : keySet) {
-            readableCache.put(s, isMostLikelyReadable(s, path));
-        }
-
-        // Check external input readers first
-        for (String s : readableCache.keySet()) {
-            try {
-                if (readableCache.get(s) != null && readableCache.get(s)) {
-                    LOG.info("Try reading input {} with EXTERNAL inputreader '{}'...", path, s);
-                    return getInputReader(s).read(path, inputCharset, additionalArguments);
-                }
-            } catch (InputReaderException e) {
-                LOG.debug(
-                    "Was not able to read input {} with EXTERNAL inputreader '{}' although it was reported to be most likely readable. Trying next input reader...",
-                    path, s, e);
-            } catch (Throwable e) {
-                LOG.debug(
-                    "While reading the input {} with the EXTERNAL inputreader {}, an Exception occured. Trying next input reader...",
-                    path, s, e);
+            readable = getValidInputReader(path, inputCharset, readableCache, s, additionalArguments);
+            if (readable != null) {
+                return readable;
             }
+            readableCache.put(s, isMostLikelyReadable(s, path));
         }
 
         // If no external input reader was found, check internal input readers
         for (String s : readableCache.keySet()) {
-            try {
-                if (readableCache.get(s) == null) {
-                    LOG.info("Try reading input {} with DEFAULT inputreader '{}'...", path, s);
-                    return getInputReader(s).read(path, inputCharset, additionalArguments);
-                }
-            } catch (InputReaderException e) {
-                LOG.debug(
-                    "Was not able to read input {} with DEFAULT inputreader '{}' although it was reported to be most likely readable. Trying next input reader...",
-                    path, s, e);
-            } catch (Throwable e) {
-                LOG.debug(
-                    "While reading the input {} with the DEFAULT inputreader {}, an Exception occured. Trying next input reader...",
-                    path, s, e);
+            readable = getValidInputReader(path, inputCharset, readableCache, s, additionalArguments);
+            if (readable != null) {
+                return readable;
             }
         }
+
         throw new InputReaderException("Could not read input at path " + path + " with any installed plugin.");
+
+    }
+
+    /**
+     * Checks and returns a valid input reader either directly or from a provided cache.
+     *
+     * @param path
+     *            the Path to the object. Can also point to a folder
+     * @param inputCharset
+     *            of the input to be used
+     * @param readableCache
+     *            HashMap of TriggerInterpreter and Boolean
+     * @param s
+     *            TriggerInterpreter
+     * @param additionalArguments
+     *            depending on the InputReader implementation
+     * @return Object that is a valid input or null if the file cannot be read by any InputReader
+     */
+    private Object getValidInputReader(Path path, Charset inputCharset, Map<String, Boolean> readableCache, String s,
+        Object... additionalArguments) {
+        String readerType = "EXTERNAL";
+        try {
+            if (isMostLikelyReadable(s, path) != null && isMostLikelyReadable(s, path)) {
+                LOG.info("Try reading input {} with {} inputreader '{}'...", path, readerType, s);
+                return getInputReader(s).read(path, inputCharset, additionalArguments);
+            }
+            if (!readableCache.isEmpty()) {
+                if (readableCache.get(s) != null && readableCache.get(s)) {
+                    LOG.info("Try reading input {} with {} inputreader '{}'...", path, readerType, s);
+                    return getInputReader(s).read(path, inputCharset, additionalArguments);
+                }
+                if (readableCache.get(s) == null) {
+                    readerType = "INTERNAL";
+                    LOG.info("Try reading input {} with {} inputreader '{}'...", path, readerType, s);
+                    return getInputReader(s).read(path, inputCharset, additionalArguments);
+                }
+            }
+
+        } catch (InputReaderException e) {
+            LOG.debug(
+                "Was not able to read input {} with {} inputreader '{}' although it was reported to be most likely readable. Trying next input reader...",
+                path, readerType, s, e);
+        } catch (Throwable e) {
+            LOG.debug(
+                "While reading the input {} with the {} inputreader {}, an Exception occured. Trying next input reader...",
+                path, readerType, s, e);
+        }
+        return null;
     }
 
     @Override
