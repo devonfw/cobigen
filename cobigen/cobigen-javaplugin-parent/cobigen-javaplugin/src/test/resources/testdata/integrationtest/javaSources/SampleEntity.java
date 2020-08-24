63a18a3539c3b5de00742fc1338009fbcdc66d1f
dchiarad <daniel.chiaradia@capgemini.com>
2020-08-24 10:05:44 +0200

    Fixed NPE when parsing a constructor with annotated parameter

--- cobigen/cobigen-javaplugin-parent/cobigen-javaplugin/src/test/resources/testdata/integrationtest/javaSources/SampleEntity.java
+++ cobigen/cobigen-javaplugin-parent/cobigen-javaplugin/src/test/resources/testdata/integrationtest/javaSources/SampleEntity.java
@@ -1,10 +1,13 @@
 package io.oasp.gastronomy.restaurant.tablemanagement.dataaccess.api;
 
+import javax.annotation.Nonnull;
 /**
  * Sample Entity without detail variable assignment variables.detail.
  */
 public class SampleEntity {
 
     private List<String> testField;
-
+    
+    public SampleEntity(@Nonnull io.oasp.gastronomy.restaurant.tablemanagement.dataaccess.api.SampleEntity sampleEntity) {
+    }
 }
