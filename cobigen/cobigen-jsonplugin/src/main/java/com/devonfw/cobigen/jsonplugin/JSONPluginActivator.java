cbe67f4bbbd6e905337a4102d41157a0b9ce2438
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2020-09-10 00:07:49 +0200

    #1228 adapt to new lazy activation API

--- cobigen/cobigen-jsonplugin/src/main/java/com/devonfw/cobigen/jsonplugin/JSONPluginActivator.java
+++ cobigen/cobigen-jsonplugin/src/main/java/com/devonfw/cobigen/jsonplugin/JSONPluginActivator.java
@@ -2,6 +2,7 @@ package com.devonfw.cobigen.jsonplugin;
 
 import java.util.List;
 
+import com.devonfw.cobigen.api.annotation.Activation;
 import com.devonfw.cobigen.api.extension.GeneratorPluginActivator;
 import com.devonfw.cobigen.api.extension.Merger;
 import com.devonfw.cobigen.api.extension.TriggerInterpreter;
@@ -11,13 +12,20 @@ import com.google.common.collect.Lists;
 /**
  * JSON Plug-in Activator to be registered in the PluginRegistry of CobiGen.
  */
+@Activation(byMergeStrategy = { JSONPluginActivator.JSONMERGE, JSONPluginActivator.JSONMERGE_OVERRIDE })
 public class JSONPluginActivator implements GeneratorPluginActivator {
+
+    /** JSON merge (prefer patch) */
+    static final String JSONMERGE_OVERRIDE = "jsonmerge_override";
+
+    /** JSON merge (prefer base) */
+    static final String JSONMERGE = "jsonmerge";
 
     @Override
     public List<Merger> bindMerger() {
         List<Merger> merger = Lists.newLinkedList();
-        merger.add(new JSONMerger("jsonmerge", false));
-        merger.add(new JSONMerger("jsonmerge_override", true));
+        merger.add(new JSONMerger(JSONMERGE, false));
+        merger.add(new JSONMerger(JSONMERGE_OVERRIDE, true));
         return merger;
     }
 
