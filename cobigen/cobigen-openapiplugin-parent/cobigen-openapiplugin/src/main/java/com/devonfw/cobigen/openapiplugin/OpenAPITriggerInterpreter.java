f331f18a97df0273df6f024e5959f8e18289e7f7
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2020-09-10 00:35:02 +0200

    #1228 adapt to new lazy activation API

--- cobigen/cobigen-openapiplugin-parent/cobigen-openapiplugin/src/main/java/com/devonfw/cobigen/openapiplugin/OpenAPITriggerInterpreter.java
+++ cobigen/cobigen-openapiplugin-parent/cobigen-openapiplugin/src/main/java/com/devonfw/cobigen/openapiplugin/OpenAPITriggerInterpreter.java
@@ -1,7 +1,9 @@
 package com.devonfw.cobigen.openapiplugin;
 
+import com.devonfw.cobigen.api.annotation.ReaderPriority;
 import com.devonfw.cobigen.api.extension.InputReader;
 import com.devonfw.cobigen.api.extension.MatcherInterpreter;
+import com.devonfw.cobigen.api.extension.Priority;
 import com.devonfw.cobigen.api.extension.TriggerInterpreter;
 import com.devonfw.cobigen.openapiplugin.inputreader.OpenAPIInputReader;
 import com.devonfw.cobigen.openapiplugin.matcher.OpenAPIMatcher;
@@ -9,6 +11,7 @@ import com.devonfw.cobigen.openapiplugin.matcher.OpenAPIMatcher;
 /**
  * {@link TriggerInterpreter} implementation of a Swagger Interpreter
  */
+@ReaderPriority(Priority.LOW)
 public class OpenAPITriggerInterpreter implements TriggerInterpreter {
 
     /**
