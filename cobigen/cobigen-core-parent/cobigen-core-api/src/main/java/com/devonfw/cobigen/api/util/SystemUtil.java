023ac0d128b5c6ccaec726f190a180142c688bdc
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2020-09-30 14:39:23 +0200

    #1252 changed determineLineDelimiter input from InputStream to Path added consolidateLineEndings method to StringUtil

--- cobigen/cobigen-core-parent/cobigen-core-api/src/main/java/com/devonfw/cobigen/api/util/SystemUtil.java
+++ cobigen/cobigen-core-parent/cobigen-core-api/src/main/java/com/devonfw/cobigen/api/util/SystemUtil.java
@@ -1,8 +1,10 @@
 package com.devonfw.cobigen.api.util;
 
 import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Path;
 
 /**
  * This util class provides system properties
@@ -22,36 +24,45 @@ public class SystemUtil {
     /**
      * Determines the line delimiter
      *
-     * @param bis
-     *            The {@link BufferedInputStream} containing the input file
-     * @param reader
-     *            The {@link InputStreamReader} iterating over the Stream
+     * @param path
+     *            The path containing the input file
+     * @param targetCharset
+     *            target char set of the file to be read and write
      * @return The line delimiter corresponding to the input file
      * @throws IOException
      *             If an exception occurs while processing the {@link BufferedInputStream} or the
      *             {@link InputStreamReader}
      */
-    public static String determineLineDelimiter(BufferedInputStream bis, InputStreamReader reader) throws IOException {
+    public static String determineLineDelimiter(Path path, String targetCharset) throws IOException {
 
-        bis.mark(0);
-        try {
-            while (reader.ready()) {
-                int nextChar = reader.read();
-                if (nextChar == '\r') {
-                    nextChar = reader.read();
-                    if (nextChar == '\n') {
-                        return "\r\n";
+        try (FileInputStream stream = new FileInputStream(path.toString());
+            BufferedInputStream bis = new BufferedInputStream(stream);
+            InputStreamReader reader = new InputStreamReader(bis, targetCharset)) {
+
+            bis.mark(0);
+            try {
+                while (reader.ready()) {
+                    int nextChar = reader.read();
+                    if (nextChar == '\r') {
+                        nextChar = reader.read();
+                        if (nextChar == '\n') {
+                            return "\r\n";
+                        }
+                        return "\r";
+                    } else if (nextChar == '\n') {
+                        return "\n";
                     }
-                    return "\r";
-                } else if (nextChar == '\n') {
-                    return "\n";
                 }
+                return null;
+            } finally {
+                emptyReader(reader);
+                bis.reset();
             }
-            return null;
-        } finally {
-            emptyReader(reader);
-            bis.reset();
+
+        } catch (IOException e) {
+            throw new IOException("Could not read file:" + path.toString(), e);
         }
+
     }
 
     /**
