366a68db23a47cc542669b500aa083878d2e8a0c
Lilli <caothivananh98@gmail.com>
2020-10-06 15:35:09 +0200

    search for maven home properly

--- cobigen-cli/cli/src/main/java/com/devonfw/cobigen/cli/utils/CobiGenUtils.java
+++ cobigen-cli/cli/src/main/java/com/devonfw/cobigen/cli/utils/CobiGenUtils.java
@@ -157,10 +157,10 @@ public class CobiGenUtils {
             t1.start();
 
             try {
-                invoker.setMavenHome(new File(System.getenv("MAVEN_HOME")));
+                invoker.setMavenHome(new File(getMavenHome()));
             } catch (NullPointerException e) {
                 LOG.error(
-                    "MAVEN_HOME environment variable has not been set on your machine. CobiGen CLI needs Maven correctly configured.",
+                    "Could not determine maven home from environment variables MAVEN_HOME or M2_HOME. CobiGen CLI needs Maven correctly configured.",
                     e);
             }
             result = invoker.execute(request);
@@ -380,6 +380,21 @@ public class CobiGenUtils {
      */
     private boolean checkTemplateDepencency(String jarToAdd) {
         return jarToAdd.contains(TemplatesJarConstants.DEVON4J_TEMPLATES_ARTIFACTID);
+    }
+
+    /**
+     * Get the maven home
+     * @return maven home
+     */
+    private String getMavenHome() {
+        String m2Home = System.getenv().get("MAVEN_HOME");
+        if (m2Home == null) {
+            m2Home = System.getenv().get("M2_HOME");
+            if (m2Home == null && "true".equals(System.getenv("TRAVIS"))) {
+                m2Home = "/usr/local/maven"; // travis only
+            }
+        }
+        return m2Home;
     }
 
 }
