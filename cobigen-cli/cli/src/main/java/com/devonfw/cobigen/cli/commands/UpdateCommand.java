59ac7dd1697d0357b0def3f0ba61f07f21a3d9f7
jan-vcapgemini <59438728+jan-vcapgemini@users.noreply.github.com>
2020-10-14 12:49:37 +0200

    Update UpdateCommand.java
    adjusted comments.

--- cobigen-cli/cli/src/main/java/com/devonfw/cobigen/cli/commands/UpdateCommand.java
+++ cobigen-cli/cli/src/main/java/com/devonfw/cobigen/cli/commands/UpdateCommand.java
@@ -27,7 +27,7 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 /**
- * This class handle update command
+ * This class handles the update command
  */
 @Command(description = MessagesConstants.UPDATE_OPTION_DESCRIPTION, name = "update", aliases = { "u" },
     mixinStandardHelpOptions = true)
@@ -93,9 +93,9 @@ public class UpdateCommand implements Callable<Integer> {
     }
 
     /**
-     * This method take user input for update template
+     * This method takes the user input for updating templates
      * @param userInputPluginForUpdate
-     *            This parameter hold list of User Input
+     *            This parameter holds the list of User Input
      */
     private void userInputPluginSelection(ArrayList<String> userInputPluginForUpdate) {
         for (String userArtifact : GenerateCommand.getUserInput().split(",")) {
@@ -104,15 +104,15 @@ public class UpdateCommand implements Callable<Integer> {
     }
 
     /**
-     * This method printing the outdated plug-ins
+     * This method is printing the outdated plug-ins
      * @param localPomDependencies
-     *            This parameter hold the artificial dependencies
+     *            This parameter holds the artificial dependencies
      * @param centralMavenVersionList
-     *            This parameter hold version list
+     *            This parameter holds version list
      * @param updatePluginVersions
-     *            This parameter contain the key ,value pair of version
+     *            This parameter contains the key, value pair of versions
      * @param listOfArtifacts
-     *            this hold list of artifact id which is need to update
+     *            This holds a list of artifact ids which need to be updated
      */
     public boolean printOutdatedPlugin(List<Dependency> localPomDependencies, List<String> centralMavenVersionList,
         HashMap<String, String> updatePluginVersions, HashMap<Integer, String> listOfArtifacts) {
@@ -154,17 +154,17 @@ public class UpdateCommand implements Callable<Integer> {
     }
 
     /**
-     * This method updating the outdated plug-ins
+     * This method is updating the outdated plug-ins
      * @param localPomDependencies
-     *            This is hold the dependencies of artificial pom
+     *            This is holding the dependencies of artificial pom
      * @param listOfArtifacts
-     *            This parameter hold the value of updated plug-ins version
+     *            This parameter holds the value of updated plug-ins version
      * @param centralMavenVersionList
-     *            This parameter hold version list
+     *            This parameter holds the version list
      * @param model
-     *            This parameter hold the after pom file reader dependencies
+     *            This parameter holds the after pom file reader dependencies
      * @param userInputPluginForUpdate
-     *            This is hold user input for update plug-ins
+     *            This is holding user input for update plug-ins
      */
     public void updateOutdatedPlugins(List<Dependency> localPomDependencies, HashMap<Integer, String> listOfArtifacts,
         List<String> centralMavenVersionList, Model model, ArrayList<String> userInputPluginForUpdate) {
