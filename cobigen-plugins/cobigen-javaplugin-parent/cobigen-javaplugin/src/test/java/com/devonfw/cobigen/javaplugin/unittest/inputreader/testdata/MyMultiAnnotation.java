43dcb3e45cbeaddaae0e3e6f7d617e3077b26442
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2021-04-29 13:20:23 +0200

    fixed java test to make it independent from json lib

---
+++ cobigen-plugins/cobigen-javaplugin-parent/cobigen-javaplugin/src/test/java/com/devonfw/cobigen/javaplugin/unittest/inputreader/testdata/MyMultiAnnotation.java
@@ -0,0 +1,19 @@
+package com.devonfw.cobigen.javaplugin.unittest.inputreader.testdata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyMultiAnnotation {
+
+    public Type[] value();
+
+    public @interface Type {
+        public Class<?> value();
+
+        public String name();
+    }
+}
