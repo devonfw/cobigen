a1e08b9963f81384344943b4e42515fe976e4851
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2021-04-30 12:28:26 +0200

    optimized and corrected build

--- build.sh
+++ build.sh
@@ -1,16 +1,31 @@
 set -e
 
-# build core without system test
-mvn clean install -f cobigen --projects !cobigen-core-systemtest -DtrimStackTrace=false
+echo "##########################################"
+echo "### Cleanup Projects #####################"
+echo "##########################################"
+mvn clean -P!p2-build -T1C
 
-# build & test plugins ones
-mvn clean install -f cobigen-plugins -DtrimStackTrace=false
+echo "##########################################"
+echo "### Build & Test Core  ###################"
+echo "##########################################"
+mvn install -f cobigen --projects !cobigen-core-systemtest -DtrimStackTrace=false -T1C
 
-# build plugin p2 repositories
-mvn clean install -DskipTests p2:site -f cobigen-plugins bundle:bundle -Pp2-bundle --projects !cobigen-javaplugin-parent/cobigen-javaplugin-model,!cobigen-openapiplugin-parent/cobigen-openapiplugin-model,!:plugins-parent,!cobigen-javaplugin-parent,!cobigen-openapiplugin-parent,!cobigen-templateengines -DtrimStackTrace=false
+echo "##########################################"
+echo "### Build & Test Core Plugins ############"
+echo "##########################################"
+mvn install -f cobigen-plugins -DtrimStackTrace=false -T1C
 
-# execute core system tests
-mvn clean verify -f cobigen --projects cobigen-core-systemtest -DtrimStackTrace=false
+echo "##########################################"
+echo "### Build Core Plugins - P2 Update Sites #"
+echo "##########################################"
+mvn package -DskipTests -f cobigen-plugins bundle:bundle -Pp2-bundle --projects !cobigen-javaplugin-parent/cobigen-javaplugin-model,!cobigen-openapiplugin-parent/cobigen-openapiplugin-model,!:plugins-parent,!cobigen-javaplugin-parent,!cobigen-openapiplugin-parent,!cobigen-templateengines -DtrimStackTrace=false -T1C
+mvn install -DskipTests -f cobigen-plugins bundle:bundle -Pp2-bundle p2:site --projects !cobigen-javaplugin-parent/cobigen-javaplugin-model,!cobigen-openapiplugin-parent/cobigen-openapiplugin-model,!:plugins-parent,!cobigen-javaplugin-parent,!cobigen-openapiplugin-parent,!cobigen-templateengines -DtrimStackTrace=false -T1C
 
-# package everything. core and plugins will be skipped since they were build already
-mvn package -DskipTests -Pp2-build-photon,p2-build-stable,p2-build-experimental -DtrimStackTrace=false
+echo "##########################################"
+echo "### Package & Run E2E Tests ##############"
+echo "##########################################"
+mvn test -f cobigen/cobigen-core-systemtest -DtrimStackTrace=false
+mvn verify -f cobigen-eclipse -DtrimStackTrace=false
+mvn verify -f cobigen-cli -DtrimStackTrace=false
+mvn verify -f cobigen-maven -DtrimStackTrace=false
+
