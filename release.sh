bc687d50347982b851652cbb05b4148d62593f36
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2021-12-22 21:20:36 +0100

    #1435 fixed gh-pages p2 bundles deployment

--- release.sh
+++ release.sh
@@ -162,8 +162,11 @@ then
   exit 0
 else
   log_step "Publish Release"
+  doRunCommand "cd ../gh-pages"
+  doRunCommand "git commit -m'Deploy P2 Bundles for Release $NEW_VERSION'"
+  doRunCommand "git push origin gh-pages"
+  doRunCommand "cd $SCRIPT_PATH"
   # Remove GA auth header in case of CI (workaround): https://github.community/t/how-to-push-to-protected-branches-in-a-github-action/16101/47
-  doRunCommand "cd ../gh-pages && git push origin gh-pages && cd $SCRIPT_PATH"
   doRunCommand "git -c "http.https://github.com/.extraheader=" push origin master"
   doRunCommand "git -c "http.https://github.com/.extraheader=" push origin v$RELEASE_VERSION"
   doRunCommand "mvn nexus-staging:release $MVN_SETTINGS -f cobigen $DEBUG $BATCH_MODE"
