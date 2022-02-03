e027a3538fa45c734ab2496150623609e3ee0d6e
MikeSchumacher <mike.schumacher@capgemini.com>
2022-02-03 11:50:45 +0100

    #1462 updated test to expect an exception instead with an expected wiki link to fix the issue

--- cobigen/cobigen-core/src/test/java/com/devonfw/cobigen/unittest/config/reader/ContextConfigurationReaderTest.java
+++ cobigen/cobigen-core/src/test/java/com/devonfw/cobigen/unittest/config/reader/ContextConfigurationReaderTest.java
@@ -1,5 +1,8 @@
 package com.devonfw.cobigen.unittest.config.reader;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import java.io.File;
 import java.nio.file.Paths;
 
@@ -47,6 +50,28 @@ public class ContextConfigurationReaderTest extends AbstractUnitTest {
   }
 
   /**
+   * Tests whether an invalidConfigurationException will be thrown when both root and templateSet folders (new templates
+   * with old custom templates) contain a context.xml file.
+   *
+   * Backward Compatibility test, remove when monolithic context.xml is deprecated.
+   *
+   * @throws InvalidConfigurationException expected
+   */
+  @Test
+  public void testExceptionWithWikiLinkWhenBothTemplateSetFolderAndRoot() throws Exception {
+
+    Throwable bothPresent = assertThrows(NullPointerException.class, () -> {
+      CobiGenFactory.create(new File(testFileRootPath + "invalid_source_folder_modular_and_root").toURI());
+    });
+
+    assertEquals(bothPresent.getMessage(),
+        "You are using an old configuration of the templates in addition to new ones."
+            + " Please make sure this is not the case as both at the same time are not supported. "
+            + "For more details visit this wiki page: "
+            + "https://github.com/devonfw/cobigen/wiki/cobigen-core_configuration#update-old-config");
+  }
+
+  /**
    * Tests whether a valid configuration can be read from src/main/templates folder (old config location) Backward
    * Compatibility test, remove when monolithic context.xml is deprecated.
    *
@@ -67,18 +92,6 @@ public class ContextConfigurationReaderTest extends AbstractUnitTest {
   public void testContextLoadedFromTemplateSetFolder() throws Exception {
 
     CobiGenFactory.create(new File(testFileRootPath + "valid_source_folder_modular").toURI());
-  }
-
-  /**
-   * Tests whether a valid configuartion can be read from both root and templateSet folders (new templates with old
-   * custom templates) Backward Compatibility test, remove when monolithic context.xml is deprecated.
-   *
-   * @throws Exception test fails
-   */
-  @Test
-  public void testContextLoadedFromTemplateSetFolderAndRoot() throws Exception {
-
-    CobiGenFactory.create(new File(testFileRootPath + "valid_source_folder_modular_and_root").toURI());
   }
 
   /**
