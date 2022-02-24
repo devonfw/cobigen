ce793e089ff041140107a370dfaf23dd351079f9
MikeSchumacher <mike.schumacher@capgemini.com>
2022-02-24 11:16:56 +0100

    changed openapiplugin mandatory warning/error message creation WIP

--- cobigen-plugins/cobigen-openapiplugin-parent/cobigen-openapiplugin/src/main/java/com/devonfw/cobigen/openapiplugin/util/constants/Constants.java
+++ cobigen-plugins/cobigen-openapiplugin-parent/cobigen-openapiplugin/src/main/java/com/devonfw/cobigen/openapiplugin/util/constants/Constants.java
@@ -18,16 +18,14 @@ public class Constants {
 
   public static final String SCHEMA = "schema";
 
-  public static String getMandatoryErrorMessage(String property) {
+  private static final String message = "The property %s was %s in a variable assignment although the input does not provide this property%s";
 
-    return "The property " + property.toLowerCase()
-        + " was required in a variable assignment although the input does not provide this property. "
-        + "Please add the required attribute in your input file or set the \"mandatory\" attribute to \"false\". ";
-  }
+  public static String getMandatoryMessage(boolean mandatory, String property) {
 
-  public static String getMandatoryWarning(String property) {
-
-    return "The property " + property.toLowerCase()
-        + " was requested in a variable assignment although the input does not provide this property. Setting it to empty";
+    if (mandatory) {
+      return String.format(message, property, "required",
+          "Please add the required attribute in your input file or set the \"mandatory\" attribute to \"false\".");
+    }
+    return String.format(message, property, "requested", ".");
   }
 }
