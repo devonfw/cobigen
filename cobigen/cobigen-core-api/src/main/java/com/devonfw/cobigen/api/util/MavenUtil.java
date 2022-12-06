3d1bded645fb7708547bc52037c0338b1711b249
EduardKrieger <eduard.krieger@capgemini.com>
2022-12-06 16:24:50 +0100

    test

--- cobigen/cobigen-core-api/src/main/java/com/devonfw/cobigen/api/util/MavenUtil.java
+++ cobigen/cobigen-core-api/src/main/java/com/devonfw/cobigen/api/util/MavenUtil.java
@@ -247,7 +247,7 @@ public class MavenUtil {
     // if (MAVEN_REPOSITORY != null) {
     // LOG.debug("Already determined {} as maven repository path.", MAVEN_REPOSITORY);
     // return MAVEN_REPOSITORY;
-    // }
+    // }//
     LOG.info("Determine maven repository path");
     if (!Strings.isNullOrEmpty(System.getenv(MavenConstants.M2_REPO_SYSTEMVARIABLE))) {
       MAVEN_REPOSITORY = Paths.get(System.getenv(MavenConstants.M2_REPO_SYSTEMVARIABLE));
