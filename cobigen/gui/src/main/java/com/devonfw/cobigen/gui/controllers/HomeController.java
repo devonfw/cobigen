c1578ca42ed63b6d01c67ef3952e94668113937a
leonie <leonie-heidi.schuessler@capgemini.com>
2022-12-14 13:51:40 +0100

    added dark theme functionality, changed detail page appearance

--- cobigen/gui/src/main/java/com/devonfw/cobigen/gui/controllers/HomeController.java
+++ cobigen/gui/src/main/java/com/devonfw/cobigen/gui/controllers/HomeController.java
@@ -11,6 +11,9 @@ import com.devonfw.cobigen.gui.services.TreeViewBuilder;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.AnchorPane;
 
@@ -23,8 +26,22 @@ public class HomeController implements Initializable {
   @FXML
   AnchorPane treeViewPane;
 
+  @FXML
+  MenuButton settings;
+
+  @FXML
+  MenuItem light;
+
+  @FXML
+  MenuItem dark;
+
   private String[] EXAMPLE_LIST = { "Title of Increment 1", "Description of Increment 1", "Title of Increment 2",
   "Description of Increment 2", "Title of Increment 3", "Description of Increment 3" };
+
+  // TODO: get path dynamically from file name
+  private String lightTheme = "file:/C:/projects/my-project/workspaces/main/cobigen/cobigen/gui/eclipse-target/classes/com/devonfw/cobigen/gui/styles.css";
+
+  private String darkTheme = "file:/C:/projects/my-project/workspaces/main/cobigen/cobigen/gui/eclipse-target/classes/com/devonfw/cobigen/gui/dark_theme.css";
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -47,5 +64,25 @@ public class HomeController implements Initializable {
   public void openWiki(javafx.event.ActionEvent actionEvent) throws IOException, URISyntaxException {
 
     Desktop.getDesktop().browse(new URI("https://github.com/devonfw/cobigen/wiki"));
+  }
+
+  public void changeToDark(javafx.event.ActionEvent actionEvent) throws IOException {
+
+    Scene scene = this.treeViewPane.getScene();
+    scene.getStylesheets().remove(this.lightTheme);
+    if (!scene.getStylesheets().contains(this.darkTheme)) {
+      scene.getStylesheets().add(this.darkTheme);
+    }
+
+  }
+
+  public void changeToLight(javafx.event.ActionEvent actionEvent) throws IOException {
+
+    Scene scene = this.treeViewPane.getScene();
+    scene.getStylesheets().remove(this.darkTheme);
+    if (!scene.getStylesheets().contains(this.lightTheme)) {
+      scene.getStylesheets().add(this.lightTheme);
+    }
+
   }
 }
