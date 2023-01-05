b76c231efbe51a52005a66664e6e75ed524ef98a
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2023-01-05 09:15:44 +0100

    upgraded openapi parser to latest version of networknt

--- cobigen-plugins/cobigen-openapiplugin-parent/cobigen-openapiplugin-model/src/main/java/com/devonfw/cobigen/openapiplugin/model/OpenAPIFile.java
+++ cobigen-plugins/cobigen-openapiplugin-parent/cobigen-openapiplugin-model/src/main/java/com/devonfw/cobigen/openapiplugin/model/OpenAPIFile.java
@@ -3,62 +3,66 @@ package com.devonfw.cobigen.openapiplugin.model;
 import java.net.URI;
 import java.nio.file.Path;
 
-import com.reprezen.kaizen.oasparser.model3.OpenApi3;
+import com.networknt.oas.model.OpenApi3;
 
-/** Swagger file transfer object which contains the {@link Path} and the name of the file */
+/**
+ * Swagger file transfer object which contains the {@link Path} and the name of
+ * the file
+ */
 public class OpenAPIFile {
 
-  /** {@link Path} to the Swagger file */
-  private Path location;
+    /** {@link Path} to the Swagger file */
+    private Path location;
 
-  /** Open API AST */
-  private OpenApi3 ast;
+    /** Open API AST */
+    private OpenApi3 ast;
 
-  /**
-   * Creates a new transfer object for a file
-   *
-   * @param location {@link URI} to the file
-   * @param ast {@link OpenApi3} AST
-   */
-  public OpenAPIFile(Path location, OpenApi3 ast) {
+    /**
+     * Creates a new transfer object for a file
+     *
+     * @param location {@link URI} to the file
+     * @param ast      {@link OpenApi3} AST
+     */
+    public OpenAPIFile(Path location, OpenApi3 ast) {
 
-    this.location = location;
-    this.ast = ast;
-  }
+        this.location = location;
+        this.ast = ast;
+    }
 
-  /**
-   * Returns the file location {@link Path}
-   *
-   * @return the file location {@link Path}
-   */
-  public Path getLocation() {
+    /**
+     * Returns the file location {@link Path}
+     *
+     * @return the file location {@link Path}
+     */
+    public Path getLocation() {
 
-    return this.location;
-  }
+        return this.location;
+    }
 
-  /**
-   * Returns the file's name
-   *
-   * @return the file's name
-   */
-  public String getFileName() {
+    /**
+     * Returns the file's name
+     *
+     * @return the file's name
+     */
+    public String getFileName() {
 
-    return this.location.getFileName().toString();
-  }
+        return this.location.getFileName().toString();
+    }
 
-  @Override
-  public String toString() {
+    @Override
+    public String toString() {
 
-    return getClass().getSimpleName() + "[fileName=" + getFileName() + ", location=" + this.location.toString() + "]";
-  }
+        return getClass().getSimpleName() + "[fileName=" + getFileName() + ", location=" + this.location.toString()
+                + "]";
+    }
 
-  /**
-   * The swagger AST of the file
-   *
-   * @return the swagger AST
-   */
-  public OpenApi3 getAST() {
+    /**
+     * The swagger AST of the file
+     *
+     * @return the swagger AST
+     */
+    public OpenApi3 getAST() {
 
-    return this.ast;
-  }
+        return this.ast;
+    }
 }
