a53b75d47004477f505e3321b821eae4c1b8578c
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2023-03-16 14:52:39 +0100

    added TODO

--- cobigen-cli/cli-systemtest/src/test/java/com/devonfw/cobigen/cli/systemtest/GenerateCommandIT.java
+++ cobigen-cli/cli-systemtest/src/test/java/com/devonfw/cobigen/cli/systemtest/GenerateCommandIT.java
@@ -127,7 +127,8 @@ public class GenerateCommandIT extends AbstractCliTest {
     assertThat(baseProject.toPath().resolve("src/main/java/com/maven/project/sampledatamanagement/logic/api/to"))
         .exists();
   }
-  // selben Test mit einer Jar
+
+  // TODO: Add similar test with a template-set jar file in downloaded folder
 
   /**
    * Integration test of the generation from a templates jar using a utility class with an extra dependency. See:
