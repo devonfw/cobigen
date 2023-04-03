a25b371b994d01f3e94733f674946f5f22bcf273
jan-vcapgemini <jan-vincent.hoelzle@capgemini.com>
2023-04-03 16:24:05 +0200

    implemented requested changes removed catch of NotYetSupportedException from TemplateSetConfigurationReader validate process

--- cobigen/cobigen-core/src/main/java/com/devonfw/cobigen/impl/config/reader/TemplateSetConfigurationReader.java
+++ cobigen/cobigen-core/src/main/java/com/devonfw/cobigen/impl/config/reader/TemplateSetConfigurationReader.java
@@ -20,7 +20,6 @@ import org.xml.sax.SAXParseException;
 
 import com.devonfw.cobigen.api.constants.ConfigurationConstants;
 import com.devonfw.cobigen.api.exception.InvalidConfigurationException;
-import com.devonfw.cobigen.api.exception.NotYetSupportedException;
 import com.devonfw.cobigen.api.util.ExceptionUtil;
 import com.devonfw.cobigen.api.util.JvmUtil;
 import com.devonfw.cobigen.impl.config.constant.MavenMetadata;
@@ -204,11 +203,7 @@ public class TemplateSetConfigurationReader {
               "The required 'version' attribute of node \"templateSetConfiguration\" has not been set");
         } else {
           VersionValidator validator = new VersionValidator(Type.TEMPLATE_SET_CONFIGURATION, MavenMetadata.VERSION);
-          try {
-            validator.validate(configVersion.floatValue());
-          } catch (NotYetSupportedException e) {
-            // TODO
-          }
+          validator.validate(configVersion.floatValue());
         }
       } else {
         throw new InvalidConfigurationException(this.templateSetFile,
